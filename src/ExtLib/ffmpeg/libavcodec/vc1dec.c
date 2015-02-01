8298ab2e9e95fae7eddff9a846737babbeabe3d7
Aleksoid1978 <Aleksoid1978@mail.ru>
2015-02-01 10:53:28 +0000

    [r70] Изменение : ffmpeg -> декодирование VC1 - после flush обрабатывать данные только начиная с AV_PICTURE_TYPE_I(Intra) фрейма. Это предотвращает падение на Intel в режиме DXVA на некоторых файлах.

--- src/ExtLib/ffmpeg/libavcodec/vc1dec.c
+++ src/ExtLib/ffmpeg/libavcodec/vc1dec.c
@@ -985,6 +985,15 @@ static int vc1_decode_frame(AVCodecContext *avctx, void *data,
     s->current_picture.f->pict_type = s->pict_type;
     s->current_picture.f->key_frame = s->pict_type == AV_PICTURE_TYPE_I;
 
+    // ==> Start patch MPC
+    if (!v->recovered && !(avctx->flags2 & CODEC_FLAG2_SHOW_ALL)) {
+        if (s->pict_type == AV_PICTURE_TYPE_I)
+            v->recovered = 1;
+        else
+            goto err;
+    }
+    // ==> End patch MPC
+
     /* skip B-frames if we don't have reference frames */
     if (!s->last_picture_ptr && (s->pict_type == AV_PICTURE_TYPE_B || s->droppable)) {
         av_log(v->s.avctx, AV_LOG_DEBUG, "Skipping B frame without reference frames\n");
@@ -1257,6 +1266,16 @@ err:
     return -1;
 }
 
+// ==> Start patch MPC
+static void vc1_decode_flush(AVCodecContext *avctx)
+{
+    VC1Context *v = avctx->priv_data;
+
+    ff_mpeg_flush(avctx);
+
+    v->recovered = 0;
+}
+// ==> End patch MPC
 
 static const AVProfile profiles[] = {
     { FF_PROFILE_VC1_SIMPLE,   "Simple"   },
@@ -1289,7 +1308,9 @@ AVCodec ff_vc1_decoder = {
     .init           = vc1_decode_init,
     .close          = ff_vc1_decode_end,
     .decode         = vc1_decode_frame,
-    .flush          = ff_mpeg_flush,
+    // ==> Start patch MPC
+    .flush          = vc1_decode_flush/*ff_mpeg_flush*/,
+    // ==> End patch MPC
     .capabilities   = CODEC_CAP_DR1 | CODEC_CAP_DELAY,
     .pix_fmts       = vc1_hwaccel_pixfmt_list_420,
     .profiles       = NULL_IF_CONFIG_SMALL(profiles)
@@ -1305,7 +1326,9 @@ AVCodec ff_wmv3_decoder = {
     .init           = vc1_decode_init,
     .close          = ff_vc1_decode_end,
     .decode         = vc1_decode_frame,
-    .flush          = ff_mpeg_flush,
+    // ==> Start patch MPC
+    .flush          = vc1_decode_flush/*ff_mpeg_flush*/,
+    // ==> End patch MPC
     .capabilities   = CODEC_CAP_DR1 | CODEC_CAP_DELAY,
     .pix_fmts       = vc1_hwaccel_pixfmt_list_420,
     .profiles       = NULL_IF_CONFIG_SMALL(profiles)
