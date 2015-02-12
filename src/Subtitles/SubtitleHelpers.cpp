4335f2657a54a77b6645ec8f1dc4a64a65507c3c
aleksoid <aleksoid@7426f7ed-705a-47fc-8cda-d59144bb0dce>
2015-02-12 01:43:39 +0000

    Изменение : Исправлена авто-загрузка субтитров при открытии файлов с сетевого пути (путь начинается с "\\").
    git-svn-id: svn://svn.code.sf.net/p/mpcbe/code/trunk@105 7426f7ed-705a-47fc-8cda-d59144bb0dce

--- src/Subtitles/SubtitleHelpers.cpp
+++ src/Subtitles/SubtitleHelpers.cpp
@@ -90,9 +90,11 @@ void Subtitle::GetSubFileNames(CString fn, const CAtlArray<CString>& paths, CAtl
 				path += '/';
 			}
 
+			/*
 			if (path.Find(':') == -1 && path.Find(_T("\\\\")) != 0) {
 				path = orgpath + path;
 			}
+			*/
 
 			path.Replace(_T("/./"), _T("/"));
 			path.Replace('/', '\\');
