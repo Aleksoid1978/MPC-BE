ef7df6849397c5ff642c66fa2f5732ad42cc278a
aleksoid <aleksoid@7426f7ed-705a-47fc-8cda-d59144bb0dce>
2015-04-18 09:18:14 +0000

    Изменение : Небольшое исправление ffmpeg/libavcodec/utils.c -> guess_correct_pts(), совпадающие pts/dts смежных пакетов не считаем "плохими".
    git-svn-id: svn://svn.code.sf.net/p/mpcbe/code/trunk@310 7426f7ed-705a-47fc-8cda-d59144bb0dce

--- src/ExtLib/ffmpeg/libavcodec/utils.c
+++ src/ExtLib/ffmpeg/libavcodec/utils.c
@@ -2205,13 +2205,19 @@ static int64_t guess_correct_pts(AVCodecContext *ctx,
     int64_t pts = AV_NOPTS_VALUE;
 
     if (dts != AV_NOPTS_VALUE) {
-        ctx->pts_correction_num_faulty_dts += dts <= ctx->pts_correction_last_dts;
+        // ==> Start patch MPC
+        // ctx->pts_correction_num_faulty_dts += dts <= ctx->pts_correction_last_dts;
+        ctx->pts_correction_num_faulty_dts += dts < ctx->pts_correction_last_dts;
+        // ==> End patch MPC
         ctx->pts_correction_last_dts = dts;
     } else if (reordered_pts != AV_NOPTS_VALUE)
         ctx->pts_correction_last_dts = reordered_pts;
 
     if (reordered_pts != AV_NOPTS_VALUE) {
-        ctx->pts_correction_num_faulty_pts += reordered_pts <= ctx->pts_correction_last_pts;
+        // ==> Start patch MPC
+        // ctx->pts_correction_num_faulty_pts += reordered_pts <= ctx->pts_correction_last_pts;
+        ctx->pts_correction_num_faulty_pts += reordered_pts < ctx->pts_correction_last_pts;
+        // ==> End patch MPC
         ctx->pts_correction_last_pts = reordered_pts;
     } else if(dts != AV_NOPTS_VALUE)
         ctx->pts_correction_last_pts = dts;
