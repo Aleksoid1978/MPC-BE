5bd737d163ae813bcb55a199f5e337084bf0ee2a
Aleksoid1978 <Aleksoid1978@mail.ru>
2015-05-20 23:07:28 +0000

    [r414] Изменение : Удалены ненужные вызовы функции SystemParametersInfo().

--- src/apps/mplayerc/MainFrm.cpp
+++ src/apps/mplayerc/MainFrm.cpp
@@ -2570,22 +2570,6 @@ void CMainFrame::OnTimer(UINT_PTR nIDEvent)
 
 				m_wndInfoBar.SetLine(ResStr(IDS_INFOBAR_SUBTITLES), Subtitles);
 			}
-
-			if (GetMediaState() == State_Running && !m_bAudioOnly) {
-				UINT fSaverActive = 0;
-				if (SystemParametersInfo(SPI_GETSCREENSAVEACTIVE, 0, (PVOID)&fSaverActive, 0)) {
-					SystemParametersInfo(SPI_SETSCREENSAVEACTIVE, 0, 0, SPIF_SENDWININICHANGE); // this might not be needed at all...
-					SystemParametersInfo(SPI_SETSCREENSAVEACTIVE, fSaverActive, 0, SPIF_SENDWININICHANGE);
-				}
-
-				fSaverActive = 0;
-				if (SystemParametersInfo(SPI_GETPOWEROFFACTIVE, 0, (PVOID)&fSaverActive, 0)) {
-					SystemParametersInfo(SPI_SETPOWEROFFACTIVE, 0, 0, SPIF_SENDWININICHANGE); // this might not be needed at all...
-					SystemParametersInfo(SPI_SETPOWEROFFACTIVE, fSaverActive, 0, SPIF_SENDWININICHANGE);
-				}
-				// prevent screensaver activate, monitor sleep/turn off after playback
-				// SetThreadExecutionState(ES_SYSTEM_REQUIRED | ES_DISPLAY_REQUIRED);
-			}
 		}
 		break;
 		case TIMER_STATUSERASER: {
