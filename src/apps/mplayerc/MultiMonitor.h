6a8b5d06c72ef359025ce65e0e472a4a0e87d337
aleksoid <aleksoid@7426f7ed-705a-47fc-8cda-d59144bb0dce>
2015-05-28 00:07:00 +0000

    Изменение : MultiMonitor::CenterRectToMonitor() - немного переделали, чтобы координаты рассчитывались аналогичные CWnd::CenterWindow(). Косметика.
    git-svn-id: svn://svn.code.sf.net/p/mpcbe/code/trunk@434 7426f7ed-705a-47fc-8cda-d59144bb0dce

--- src/apps/mplayerc/MultiMonitor.h
+++ src/apps/mplayerc/MultiMonitor.h
@@ -4,14 +4,14 @@
  * Email: don@itsEngineering.com
  * Copyright 2002, Donald Kackman
  *
- * This file is part of mplayerc.
+ * This file is part of MPC-BE.
  *
- * Mplayerc is free software; you can redistribute it and/or modify
+ * MPC-BE is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
- * Mplayerc is distributed in the hope that it will be useful,
+ * MPC-BE is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
