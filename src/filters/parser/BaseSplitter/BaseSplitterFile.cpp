7ff848d1b2e8a24a5844233a11324ac0cd4e9ba4
v0lt <v0lt@7426f7ed-705a-47fc-8cda-d59144bb0dce>
2015-08-15 11:59:12 +0000

    BaseSplitter: кеш наполняем только имеющимися данными.
    git-svn-id: svn://svn.code.sf.net/p/mpcbe/code/trunk@648 7426f7ed-705a-47fc-8cda-d59144bb0dce

--- src/filters/parser/BaseSplitter/BaseSplitterFile.cpp
+++ src/filters/parser/BaseSplitter/BaseSplitterFile.cpp
@@ -320,7 +320,7 @@ HRESULT CBaseSplitterFile::Read(BYTE* pData, __int64 len)
 	}
 
 	while (len > 0) {
-		__int64 tmplen = GetLength();
+		__int64 tmplen = GetAvailable();
 		__int64 maxlen = min(tmplen - m_pos, m_cachetotal);
 		__int64 minlen = min(len, maxlen);
 		if (minlen <= 0) {
