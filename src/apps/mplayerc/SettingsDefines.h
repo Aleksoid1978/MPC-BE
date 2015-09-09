364d110761d06f3c0f0da3e18fa2feeabf28a5a4
v0lt <v0lt@7426f7ed-705a-47fc-8cda-d59144bb0dce>
2015-09-09 03:35:21 +0000

    Добавлена опция изменения окна при воспроизведении аудиофайлов (пока не работает).
    git-svn-id: svn://svn.code.sf.net/p/mpcbe/code/trunk@741 7426f7ed-705a-47fc-8cda-d59144bb0dce

--- src/apps/mplayerc/SettingsDefines.h
+++ src/apps/mplayerc/SettingsDefines.h
@@ -144,6 +144,7 @@
 #define IDS_RS_SUBDELAYINTERVAL				_T("SubDelayInterval")
 #define IDS_RS_ENABLEAUDIOTIMESHIFT			_T("EnableAudioTimeShift")
 #define IDS_RS_LOGOFILE						_T("LogoFile")
+#define IDS_RS_AUDIOWINDOWMODE				_T("AudioWindowMode")
 #define IDS_RS_ENABLEWORKERTHREADFOROPENING	_T("EnableWorkerThreadForOpening")
 #define IDS_RS_PNSPRESETS					_T("PnSPresets")
 #define IDS_RS_AUTOLOADAUDIO				_T("AutoloadAudio")
