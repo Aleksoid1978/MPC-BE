379a86dc04801e192aba20a5c2df992ee62a187b
v0lt <mpc-be@mail.ru>
2015-09-24 06:51:36 +0000

    [r786] Удалены дефайны DIRECTX_SDK_DATE и DIRECTX_SDK_NUMBER. Регистрация и удаление регистрации расширения оболочки теперь производится инсталлятором и деинсталлятором соответсвенно.

--- src/filters/renderer/VideoRenderers/DX9AllocatorPresenter.cpp
+++ src/filters/renderer/VideoRenderers/DX9AllocatorPresenter.cpp
@@ -88,9 +88,7 @@ CDX9AllocatorPresenter::CDX9AllocatorPresenter(HWND hWnd, bool bFullscreen, HRES
 		(FARPROC&)m_pD3DXCreateFont				= GetProcAddress(hDll, "D3DXCreateFontW");
 		(FARPROC&)m_pD3DXCreateSprite			= GetProcAddress(hDll, "D3DXCreateSprite");
 	} else {
-		_Error += L"The installed DirectX End-User Runtime is outdated. Please download and install the ";
-		_Error += DIRECTX_SDK_DATE;
-		_Error += L" release or newer in order for MPC-BE to function properly.\n";
+		_Error += L"The installed DirectX End-User Runtime is outdated. Please download and install the June 2010 release or newer in order for MPC-BE to function properly.\n";
 	}
 
 	m_pDwmIsCompositionEnabled = NULL;
