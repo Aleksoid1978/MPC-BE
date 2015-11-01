c443a6b9c9278dec5d48c9aa443ae5f341c93058
v0lt <v0lt@7426f7ed-705a-47fc-8cda-d59144bb0dce>
2015-11-01 15:51:30 +0000

    Добавлено описание некоторых известных DXVA GUID. Функция GetD3DFormatStr теперь возвращает результат для D3DFMT_NV12 и D3DFMT_P010, косметика.
    git-svn-id: svn://svn.code.sf.net/p/mpcbe/code/trunk@912 7426f7ed-705a-47fc-8cda-d59144bb0dce

--- src/filters/renderer/VideoRenderers/AllocatorCommon.cpp
+++ src/filters/renderer/VideoRenderers/AllocatorCommon.cpp
@@ -178,132 +178,71 @@ CString GetWindowsErrorMessage(HRESULT _Error, HMODULE _Module)
 const wchar_t *GetD3DFormatStr(D3DFORMAT Format)
 {
 	switch (Format) {
-		case D3DFMT_R8G8B8 :
-			return L"R8G8B8";
-		case D3DFMT_A8R8G8B8 :
-			return L"A8R8G8B8";
-		case D3DFMT_X8R8G8B8 :
-			return L"X8R8G8B8";
-		case D3DFMT_R5G6B5 :
-			return L"R5G6B5";
-		case D3DFMT_X1R5G5B5 :
-			return L"X1R5G5B5";
-		case D3DFMT_A1R5G5B5 :
-			return L"A1R5G5B5";
-		case D3DFMT_A4R4G4B4 :
-			return L"A4R4G4B4";
-		case D3DFMT_R3G3B2 :
-			return L"R3G3B2";
-		case D3DFMT_A8 :
-			return L"A8";
-		case D3DFMT_A8R3G3B2 :
-			return L"A8R3G3B2";
-		case D3DFMT_X4R4G4B4 :
-			return L"X4R4G4B4";
-		case D3DFMT_A2B10G10R10:
-			return L"A2B10G10R10";
-		case D3DFMT_A8B8G8R8 :
-			return L"A8B8G8R8";
-		case D3DFMT_X8B8G8R8 :
-			return L"X8B8G8R8";
-		case D3DFMT_G16R16 :
-			return L"G16R16";
-		case D3DFMT_A2R10G10B10:
-			return L"A2R10G10B10";
-		case D3DFMT_A16B16G16R16 :
-			return L"A16B16G16R16";
-		case D3DFMT_A8P8 :
-			return L"A8P8";
-		case D3DFMT_P8 :
-			return L"P8";
-		case D3DFMT_L8 :
-			return L"L8";
-		case D3DFMT_A8L8 :
-			return L"A8L8";
-		case D3DFMT_A4L4 :
-			return L"A4L4";
-		case D3DFMT_V8U8 :
-			return L"V8U8";
-		case D3DFMT_L6V5U5 :
-			return L"L6V5U5";
-		case D3DFMT_X8L8V8U8 :
-			return L"X8L8V8U8";
-		case D3DFMT_Q8W8V8U8 :
-			return L"Q8W8V8U8";
-		case D3DFMT_V16U16 :
-			return L"V16U16";
-		case D3DFMT_A2W10V10U10:
-			return L"A2W10V10U10";
-		case D3DFMT_UYVY :
-			return L"UYVY";
-		case D3DFMT_R8G8_B8G8:
-			return L"R8G8_B8G8";
-		case D3DFMT_YUY2 :
-			return L"YUY2";
-		case D3DFMT_G8R8_G8B8:
-			return L"G8R8_G8B8";
-		case D3DFMT_DXT1 :
-			return L"DXT1";
-		case D3DFMT_DXT2 :
-			return L"DXT2";
-		case D3DFMT_DXT3 :
-			return L"DXT3";
-		case D3DFMT_DXT4 :
-			return L"DXT4";
-		case D3DFMT_DXT5 :
-			return L"DXT5";
-		case D3DFMT_D16_LOCKABLE :
-			return L"D16_LOCKABLE";
-		case D3DFMT_D32:
-			return L"D32";
-		case D3DFMT_D15S1:
-			return L"D15S1";
-		case D3DFMT_D24S8:
-			return L"D24S8";
-		case D3DFMT_D24X8:
-			return L"D24X8";
-		case D3DFMT_D24X4S4:
-			return L"D24X4S4";
-		case D3DFMT_D16:
-			return L"D16";
-		case D3DFMT_D32F_LOCKABLE:
-			return L"D32F_LOCKABLE";
-		case D3DFMT_D24FS8 :
-			return L"D24FS8";
-		case D3DFMT_D32_LOCKABLE :
-			return L"D32_LOCKABLE";
-		case D3DFMT_S8_LOCKABLE:
-			return L"S8_LOCKABLE";
-		case D3DFMT_L16:
-			return L"L16";
-		case D3DFMT_VERTEXDATA :
-			return L"VERTEXDATA";
-		case D3DFMT_INDEX16:
-			return L"INDEX16";
-		case D3DFMT_INDEX32:
-			return L"INDEX32";
-		case D3DFMT_Q16W16V16U16 :
-			return L"Q16W16V16U16";
-		case D3DFMT_MULTI2_ARGB8 :
-			return L"MULTI2_ARGB8";
-		case D3DFMT_R16F :
-			return L"R16F";
-		case D3DFMT_G16R16F:
-			return L"G16R16F";
-		case D3DFMT_A16B16G16R16F:
-			return L"A16B16G16R16F";
-		case D3DFMT_R32F :
-			return L"R32F";
-		case D3DFMT_G32R32F:
-			return L"G32R32F";
-		case D3DFMT_A32B32G32R32F:
-			return L"A32B32G32R32F";
-		case D3DFMT_CxV8U8 :
-			return L"CxV8U8";
-		case D3DFMT_A1 :
-			return L"A1";
-		case D3DFMT_BINARYBUFFER :
-			return L"BINARYBUFFER";
+		case D3DFMT_R8G8B8:			return L"R8G8B8";
+		case D3DFMT_A8R8G8B8:		return L"A8R8G8B8";
+		case D3DFMT_X8R8G8B8:		return L"X8R8G8B8";
+		case D3DFMT_R5G6B5:			return L"R5G6B5";
+		case D3DFMT_X1R5G5B5:		return L"X1R5G5B5";
+		case D3DFMT_A1R5G5B5:		return L"A1R5G5B5";
+		case D3DFMT_A4R4G4B4:		return L"A4R4G4B4";
+		case D3DFMT_R3G3B2:			return L"R3G3B2";
+		case D3DFMT_A8:				return L"A8";
+		case D3DFMT_A8R3G3B2:		return L"A8R3G3B2";
+		case D3DFMT_X4R4G4B4:		return L"X4R4G4B4";
+		case D3DFMT_A2B10G10R10:	return L"A2B10G10R10";
+		case D3DFMT_A8B8G8R8:		return L"A8B8G8R8";
+		case D3DFMT_X8B8G8R8:		return L"X8B8G8R8";
+		case D3DFMT_G16R16:			return L"G16R16";
+		case D3DFMT_A2R10G10B10:	return L"A2R10G10B10";
+		case D3DFMT_A16B16G16R16:	return L"A16B16G16R16";
+		case D3DFMT_A8P8:			return L"A8P8";
+		case D3DFMT_P8:				return L"P8";
+		case D3DFMT_L8:				return L"L8";
+		case D3DFMT_A8L8:			return L"A8L8";
+		case D3DFMT_A4L4:			return L"A4L4";
+		case D3DFMT_V8U8:			return L"V8U8";
+		case D3DFMT_L6V5U5:			return L"L6V5U5";
+		case D3DFMT_X8L8V8U8:		return L"X8L8V8U8";
+		case D3DFMT_Q8W8V8U8:		return L"Q8W8V8U8";
+		case D3DFMT_V16U16:			return L"V16U16";
+		case D3DFMT_A2W10V10U10:	return L"A2W10V10U10";
+		case D3DFMT_UYVY:			return L"UYVY";
+		case D3DFMT_R8G8_B8G8:		return L"R8G8_B8G8";
+		case D3DFMT_YUY2:			return L"YUY2";
+		case D3DFMT_G8R8_G8B8:		return L"G8R8_G8B8";
+		case D3DFMT_DXT1:			return L"DXT1";
+		case D3DFMT_DXT2:			return L"DXT2";
+		case D3DFMT_DXT3:			return L"DXT3";
+		case D3DFMT_DXT4:			return L"DXT4";
+		case D3DFMT_DXT5:			return L"DXT5";
+		case D3DFMT_D16_LOCKABLE:	return L"D16_LOCKABLE";
+		case D3DFMT_D32:			return L"D32";
+		case D3DFMT_D15S1:			return L"D15S1";
+		case D3DFMT_D24S8:			return L"D24S8";
+		case D3DFMT_D24X8:			return L"D24X8";
+		case D3DFMT_D24X4S4:		return L"D24X4S4";
+		case D3DFMT_D16:			return L"D16";
+		case D3DFMT_D32F_LOCKABLE:	return L"D32F_LOCKABLE";
+		case D3DFMT_D24FS8:			return L"D24FS8";
+		case D3DFMT_D32_LOCKABLE:	return L"D32_LOCKABLE";
+		case D3DFMT_S8_LOCKABLE:	return L"S8_LOCKABLE";
+		case D3DFMT_L16:			return L"L16";
+		case D3DFMT_VERTEXDATA:		return L"VERTEXDATA";
+		case D3DFMT_INDEX16:		return L"INDEX16";
+		case D3DFMT_INDEX32:		return L"INDEX32";
+		case D3DFMT_Q16W16V16U16:	return L"Q16W16V16U16";
+		case D3DFMT_MULTI2_ARGB8:	return L"MULTI2_ARGB8";
+		case D3DFMT_R16F:			return L"R16F";
+		case D3DFMT_G16R16F:		return L"G16R16F";
+		case D3DFMT_A16B16G16R16F:	return L"A16B16G16R16F";
+		case D3DFMT_R32F:			return L"R32F";
+		case D3DFMT_G32R32F:		return L"G32R32F";
+		case D3DFMT_A32B32G32R32F:	return L"A32B32G32R32F";
+		case D3DFMT_CxV8U8:			return L"CxV8U8";
+		case D3DFMT_A1:				return L"A1";
+		case D3DFMT_BINARYBUFFER:	return L"BINARYBUFFER";
+		case FCC('NV12'):			return L"NV12";
+		case FCC('P010'):			return L"P010";
 	}
 	return L"Unknown";
 }
