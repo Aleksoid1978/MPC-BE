91fdaa69241355a3e168f12373b26f46b74df9cc
v0lt <v0lt@7426f7ed-705a-47fc-8cda-d59144bb0dce>
2016-01-29 15:44:34 +0000

    MPCVideoDec: задаем "начальный" цветовой формат для Сanopus HQ/HQX для дальнейшей корректной инициализации выходного пина декодера.
    git-svn-id: svn://svn.code.sf.net/p/mpcbe/code/trunk@1147 7426f7ed-705a-47fc-8cda-d59144bb0dce

--- src/filters/transform/MPCVideoDec/ffmpegContext.cpp
+++ src/filters/transform/MPCVideoDec/ffmpegContext.cpp
@@ -567,15 +567,19 @@ void FillAVCodecProps(struct AVCodecContext* pAVCtx)
 				}
 				break;
 			case AV_CODEC_ID_PNG:
-				pAVCtx->pix_fmt = AV_PIX_FMT_RGBA;
+				pAVCtx->pix_fmt = AV_PIX_FMT_RGBA; // and other RGB formats, but it is not important here
+				break;
+			case AV_CODEC_ID_HQ_HQA:
+				pAVCtx->pix_fmt = AV_PIX_FMT_YUV422P;
+				break;
+			case AV_CODEC_ID_HQX:
+				pAVCtx->pix_fmt = AV_PIX_FMT_YUV422P16; // or AV_PIX_FMT_YUV444P16
 				break;
 			case AV_CODEC_ID_MJPEG:
 			case AV_CODEC_ID_DNXHD:
-				av_log(pAVCtx, AV_LOG_INFO, "WARNING! : pAVCtx->pix_fmt == AV_PIX_FMT_NONE\n");
-				pAVCtx->pix_fmt = AV_PIX_FMT_YUV422P; // bad hack
+				pAVCtx->pix_fmt = AV_PIX_FMT_YUV422P; // most common format for these codecs
 				break;
-			default:
-				av_log(pAVCtx, AV_LOG_INFO, "WARNING! : pAVCtx->pix_fmt == AV_PIX_FMT_NONE\n");
+			default: // most codecs
 				pAVCtx->pix_fmt = AV_PIX_FMT_YUV420P; // bad hack
 		}
 	}
