dda36812c7fc8e7b9c21a1086c4ed3baa92b15b2
Aleksoid1978 <Aleksoid1978@mail.ru>
2016-02-14 13:16:09 +0000

    [r1174] Изменение : Добавлена поддержка dual signing для добавление SHA-256 сертификата.

--- contrib/sign.cmd
+++ contrib/sign.cmd
@@ -3,12 +3,11 @@ SETLOCAL
 
 IF "%~1" == "" (
   ECHO %~nx0: No input file specified!
-  EXIT /B 1
+  GOTO END
 )
 
 IF NOT EXIST "%~dp0signinfo.txt" (
   ECHO %~nx0: %~dp0signinfo.txt is not present!
-  SET SIGN_ERROR=True
   GOTO END
 )
 
@@ -18,21 +17,23 @@ IF DEFINED VS140COMNTOOLS (
   SET "VSCOMNTOOLS=%VS120COMNTOOLS%"
 ) ELSE (
   ECHO %~nx0: Visual Studio does not seem to be installed...
-  EXIT /B 1
+  GOTO END
 )
 
 CALL "%VSCOMNTOOLS%..\..\VC\vcvarsall.bat" x86 > nul
 
-SET SIGN_CMD=
-SET /P SIGN_CMD=<%~dp0signinfo.txt
-
 TITLE Signing "%~1"...
 ECHO. & ECHO Signing "%~1"...
 
+FOR /F "delims=" %%A IN (%~dp0signinfo.txt) DO (SET "SIGN_CMD=%%A" && CALL :SIGN %1)
+
+:END
+ENDLOCAL
+EXIT /B %ERRORLEVEL%
+
+:SIGN
 FOR /L %%i IN (1,1,5) DO (
   IF %%i GTR 1 ECHO %%i attempt
   signtool.exe sign %SIGN_CMD% "%~1"
-  IF NOT ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
+  IF %ERRORLEVEL% EQU 0 EXIT /B %ERRORLEVEL%
 )
-
-EXIT /B %ERRORLEVEL%
