fed6f0495df0daa0041be949d6c8890368907e30
v0lt <mpc-be@mail.ru>
2016-04-02 17:51:40 +0000

    [r1340] Обновлен копирайт для файлов изменившихся в 2016 году. Косметика.

--- src/apps/mplayerc/MediaFormats.h
+++ src/apps/mplayerc/MediaFormats.h
@@ -1,6 +1,6 @@
 /*
  * (C) 2003-2006 Gabest
- * (C) 2006-2014 see Authors.txt
+ * (C) 2006-2016 see Authors.txt
  *
  * This file is part of MPC-BE.
  *
@@ -22,7 +22,6 @@
 #pragma once
 
 #include <atlcoll.h>
-#include "BaseGraph.h"
 
 enum filetype_t {
 	TVideo = 0,
