41ef0cecb40ee96af0280486e6d8a381f638a49f
Aleksoid1978 <Aleksoid1978@mail.ru>
2016-04-21 04:03:08 +0000

    [r1433] Изменение : Обновление libpng git-v1.6.22beta04-17-g0392e36.

--- src/ExtLib/libpng/png.c
+++ src/ExtLib/libpng/png.c
@@ -14,7 +14,7 @@
 #include "pngpriv.h"
 
 /* Generate a compiler error if there is an old png.h in the search path. */
-typedef png_libpng_version_1_6_22beta01 Your_png_h_is_not_version_1_6_22beta01;
+typedef png_libpng_version_1_6_22beta05 Your_png_h_is_not_version_1_6_22beta05;
 
 /* Tells libpng that we have already handled the first "num_bytes" bytes
  * of the PNG file signature.  If the PNG data is embedded into another
@@ -775,14 +775,14 @@ png_get_copyright(png_const_structrp png_ptr)
 #else
 #  ifdef __STDC__
    return PNG_STRING_NEWLINE \
-      "libpng version 1.6.22beta01 - January 16, 2016" PNG_STRING_NEWLINE \
+      "libpng version 1.6.22beta05 - April 5, 2016" PNG_STRING_NEWLINE \
       "Copyright (c) 1998-2002,2004,2006-2016 Glenn Randers-Pehrson" \
       PNG_STRING_NEWLINE \
       "Copyright (c) 1996-1997 Andreas Dilger" PNG_STRING_NEWLINE \
       "Copyright (c) 1995-1996 Guy Eric Schalnat, Group 42, Inc." \
       PNG_STRING_NEWLINE;
 #  else
-   return "libpng version 1.6.22beta01 - January 16, 2016\
+   return "libpng version 1.6.22beta05 - April 5, 2016\
       Copyright (c) 1998-2002,2004,2006-2016 Glenn Randers-Pehrson\
       Copyright (c) 1996-1997 Andreas Dilger\
       Copyright (c) 1995-1996 Guy Eric Schalnat, Group 42, Inc.";
