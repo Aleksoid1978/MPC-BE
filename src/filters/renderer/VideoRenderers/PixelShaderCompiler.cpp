ac174b280d38625c921b02fd965c2baf7ff12b54
v0lt <v0lt@7426f7ed-705a-47fc-8cda-d59144bb0dce>
2016-06-18 10:59:28 +0000

    Не проверяем наличие d3dx9_43.dll и d3dcompiler_47.dll повторно, если в первый раз не нашли. (кроме некоторых случаев в режиме отладки, которые не так важны)
    git-svn-id: svn://svn.code.sf.net/p/mpcbe/code/trunk@1631 7426f7ed-705a-47fc-8cda-d59144bb0dce

--- src/filters/renderer/VideoRenderers/PixelShaderCompiler.cpp
+++ src/filters/renderer/VideoRenderers/PixelShaderCompiler.cpp
@@ -26,26 +26,22 @@
 
 HINSTANCE GetD3dcompilerDll()
 {
-	static HINSTANCE s_hD3dcompilerDll = NULL;
-
-	if (s_hD3dcompilerDll == NULL) {
-		s_hD3dcompilerDll = LoadLibrary(L"d3dcompiler_47.dll"); // Windows Kits\8.1\Redist\D3D\[x86|x64]\d3dcompiler_47.dll
+	static HINSTANCE s_hD3dcompilerDll = LoadLibraryW(L"d3dcompiler_47.dll"); // Windows Kits\8.1\Redist\D3D\[x86|x64]\d3dcompiler_47.dll
 
 #ifdef _DEBUG
-		if (s_hD3dcompilerDll == NULL) {
-			// try an absolute path (needed when running the debug version outside of Visual Studio)
-			CString path;
-			SHGetFolderPath(NULL, CSIDL_PROGRAM_FILESX86, NULL, 0, path.GetBuffer(_MAX_PATH));
-			path.ReleaseBuffer();
+	if (s_hD3dcompilerDll == NULL) {
+		// try an absolute path (needed when running the debug version outside of Visual Studio)
+		CString path;
+		SHGetFolderPath(NULL, CSIDL_PROGRAM_FILESX86, NULL, 0, path.GetBuffer(_MAX_PATH));
+		path.ReleaseBuffer();
 	#ifdef _WIN64
-			path.Append(L"\\Windows Kits\\8.1\\bin\\x64\\d3dcompiler_47.dll");
+		path.Append(L"\\Windows Kits\\8.1\\bin\\x64\\d3dcompiler_47.dll");
 	#else
-			path.Append(L"\\Windows Kits\\8.1\\bin\\x86\\d3dcompiler_47.dll");
+		path.Append(L"\\Windows Kits\\8.1\\bin\\x86\\d3dcompiler_47.dll");
 	#endif
-			s_hD3dcompilerDll = LoadLibrary(path);
-		}
-#endif
+		s_hD3dcompilerDll = LoadLibrary(path);
 	}
+#endif
 
 	return s_hD3dcompilerDll;
 }
--- src/filters/renderer/VideoRenderers/RenderersSettings.cpp
+++ src/filters/renderer/VideoRenderers/RenderersSettings.cpp
@@ -78,12 +78,7 @@ HINSTANCE GetD3X9Dll()
 #if D3DX_SDK_VERSION < 43
 #error DirectX SDK June 2010 (v43) or newer is required to build MPC-BE
 #endif
-	static HINSTANCE s_hD3DX9Dll = NULL;
-
-	if (s_hD3DX9Dll == NULL) {
-		// load latest compatible version of the DLL that is available
-		s_hD3DX9Dll = LoadLibrary(L"d3dx9_43.dll");
-	}
+	static HINSTANCE s_hD3DX9Dll = LoadLibraryW(L"d3dx9_43.dll"); // load latest compatible version of the DLL that is available
 
 	return s_hD3DX9Dll;
 }
