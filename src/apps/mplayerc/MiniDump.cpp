98e949c41797ac8fd02b4db64a367b514ff5742a
v0lt <v0lt@7426f7ed-705a-47fc-8cda-d59144bb0dce>
2016-07-17 17:54:00 +0000

    AfxGetMyApp()->GetMainWnd()->m_hWnd -> AfxGetApp()->GetMainWnd()->m_hWnd (убрано лишнее приведение типа). Косметика.
    git-svn-id: svn://svn.code.sf.net/p/mpcbe/code/trunk@1744 7426f7ed-705a-47fc-8cda-d59144bb0dce

--- src/apps/mplayerc/MiniDump.cpp
+++ src/apps/mplayerc/MiniDump.cpp
@@ -131,7 +131,7 @@ LONG WINAPI CMiniDump::UnhandledExceptionFilter( _EXCEPTION_POINTERS *lpTopLevel
 	}
 
 	if (szResult[0]) {
-		switch (MessageBox(AfxGetMyApp()->GetMainWnd()->m_hWnd, szResult, _T("MPC-BE Mini Dump"), retval ? MB_YESNO : MB_OK)) {
+		switch (MessageBox(AfxGetApp()->GetMainWnd()->m_hWnd, szResult, _T("MPC-BE Mini Dump"), retval ? MB_YESNO : MB_OK)) {
 			case IDYES:
 				ShellExecute(NULL, _T("open"), _T("http://sourceforge.net/p/mpcbe/tickets/"), NULL, NULL, SW_SHOWDEFAULT); // hmm
 				ExploreToFile(strDumpPath);
