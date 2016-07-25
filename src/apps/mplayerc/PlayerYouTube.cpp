0bba07328015a5e9bf8864208c04469e661cdf18
aleksoid <aleksoid@7426f7ed-705a-47fc-8cda-d59144bb0dce>
2016-07-25 23:50:27 +0000

    Изменение : Сохранение Youtube ссылок - в имя файла добавляем суффикс качества видео(720p/1080p и т.д.)
    git-svn-id: svn://svn.code.sf.net/p/mpcbe/code/trunk@1766 7426f7ed-705a-47fc-8cda-d59144bb0dce

--- src/apps/mplayerc/PlayerYouTube.cpp
+++ src/apps/mplayerc/PlayerYouTube.cpp
@@ -714,6 +714,7 @@ namespace Youtube
 
 			CString final_video_url = final_item->url;
 			CString final_video_ext = final_item->profile->ext;
+			CString final_video_quality_suffix; final_video_quality_suffix.Format(L"%dp", final_item->profile->quality);
 
 			CString final_audio_url;
 			if (final_item->profile->type == y_video && !AudioList.empty()) {
@@ -743,7 +744,7 @@ namespace Youtube
 
 				ParseMetadata(hInet, videoId, y_fields);
 
-				y_fields.fname = y_fields.title + L"." + final_video_ext;
+				y_fields.fname = y_fields.title + L" - " + final_video_quality_suffix + L"." + final_video_ext;
 				FixFilename(y_fields.fname);
 
 				if (!videoId.IsEmpty()) { // subtitle
