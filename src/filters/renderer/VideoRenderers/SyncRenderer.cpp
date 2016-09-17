eb8483423c7a0991ef8fd9218a7afda4555d9411
v0lt <v0lt@7426f7ed-705a-47fc-8cda-d59144bb0dce>
2016-09-17 09:10:00 +0000

    VideoRenderers: исправлена некачественная работа зума (Ctrl+Num9) после внедрения двухпроходной интерполяции в r1885.
    git-svn-id: svn://svn.code.sf.net/p/mpcbe/code/trunk@1918 7426f7ed-705a-47fc-8cda-d59144bb0dce

--- src/filters/renderer/VideoRenderers/SyncRenderer.cpp
+++ src/filters/renderer/VideoRenderers/SyncRenderer.cpp
@@ -911,9 +911,25 @@ HRESULT CBaseAP::TextureResizeShader2pass(IDirect3DTexture9* pTexture, Vector ds
 		return E_FAIL;
 	}
 
+	float w2 = sqrt(pow(dst[1].x - dst[0].x, 2) + pow(dst[1].y - dst[0].y, 2) + pow(dst[1].z - dst[0].z, 2));
+	float h2 = sqrt(pow(dst[2].x - dst[0].x, 2) + pow(dst[2].y - dst[0].y, 2) + pow(dst[2].z - dst[0].z, 2));
+
+	float rx = srcRect.Width() / w2;
+	float ry = srcRect.Height() / h2;
+
+	const float dx0 = 1.0f / desc.Width;
+	const float dy0 = 1.0f / desc.Height;
+
+	UINT texWidth = min((UINT)w2, m_Caps.MaxTextureWidth);
+	UINT texHeight = min((UINT)m_nativeVideoSize.cy, m_Caps.MaxTextureHeight);
+
+	if (m_pResizeTexture && m_pResizeTexture->GetLevelDesc(0, &desc) == D3D_OK) {
+		if (texWidth != desc.Width || texHeight != desc.Height) {
+			m_pResizeTexture = NULL;
+		}
+	}
+
 	if (!m_pResizeTexture) {
-		UINT texWidth = min((UINT)m_ScreenSize.cx, m_Caps.MaxTextureWidth);
-		UINT texHeight = min((UINT)m_nativeVideoSize.cy, m_Caps.MaxTextureHeight);
 		hr = m_pD3DDevEx->CreateTexture(
 			texWidth, texHeight, 1, D3DUSAGE_RENDERTARGET,
 			m_SurfaceFmt == D3DFMT_A32B32G32R32F ? D3DFMT_A32B32G32R32F : D3DFMT_A16B16G16R16F, // use only float textures here
@@ -923,15 +939,6 @@ HRESULT CBaseAP::TextureResizeShader2pass(IDirect3DTexture9* pTexture, Vector ds
 			return TextureResize(pTexture, dst, srcRect, D3DTEXF_LINEAR);
 		}
 	}
-
-	float w2 = sqrt(pow(dst[1].x - dst[0].x, 2) + pow(dst[1].y - dst[0].y, 2) + pow(dst[1].z - dst[0].z, 2));
-	float h2 = sqrt(pow(dst[2].x - dst[0].x, 2) + pow(dst[2].y - dst[0].y, 2) + pow(dst[2].z - dst[0].z, 2));
-
-	float rx = srcRect.Width() / w2;
-	float ry = srcRect.Height() / h2;
-
-	const float dx0 = 1.0f / desc.Width;
-	const float dy0 = 1.0f / desc.Height;
 
 	if (FAILED(m_pResizeTexture->GetLevelDesc(0, &desc))) {
 		return TextureResize(pTexture, dst, srcRect, D3DTEXF_LINEAR);
@@ -971,6 +978,10 @@ HRESULT CBaseAP::TextureResizeShader2pass(IDirect3DTexture9* pTexture, Vector ds
 	CComPtr<IDirect3DSurface9> pTempRenderTarget;
 	hr = m_pResizeTexture->GetSurfaceLevel(0, &pTempRenderTarget);
 	hr = m_pD3DDevEx->SetRenderTarget(0, pTempRenderTarget);
+
+	if (rx > 2.0f && ry > 2.0f) {
+		m_wsResizer = L"Simple averaging";
+	}
 
 	// resize width
 	hr = m_pD3DDevEx->SetTexture(0, pTexture);
