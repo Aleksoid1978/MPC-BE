a43aa9c8dc016fe4a793fbb44cd9eeef0a074370
v0lt <v0lt@7426f7ed-705a-47fc-8cda-d59144bb0dce>
2016-11-14 18:42:12 +0000

    "Перетряска" файлов в проекте Shаders. Файл final_pass.hlsl перенесен из проекта VideoRenderers в Shаders.
    git-svn-id: svn://svn.code.sf.net/p/mpcbe/code/trunk@2154 7426f7ed-705a-47fc-8cda-d59144bb0dce

--- src/Shaders/compile_shaders_ps20.cmd
+++ src/Shaders/compile_shaders_ps20.cmd
@@ -1,24 +1,26 @@
 @echo off
 echo === Compiling Pixel Shaders 2.0 ===
-set LIST=%LIST% resizer_bspline4_x.hlsl
-set LIST=%LIST% resizer_bspline4_y.hlsl
-set LIST=%LIST% resizer_mitchell4_x.hlsl
-set LIST=%LIST% resizer_mitchell4_y.hlsl
-set LIST=%LIST% resizer_catmull4_x.hlsl
-set LIST=%LIST% resizer_catmull4_y.hlsl
-set LIST=%LIST% resizer_bicubic06_x.hlsl
-set LIST=%LIST% resizer_bicubic06_y.hlsl
-set LIST=%LIST% resizer_bicubic08_x.hlsl
-set LIST=%LIST% resizer_bicubic08_y.hlsl
-set LIST=%LIST% resizer_bicubic10_x.hlsl
-set LIST=%LIST% resizer_bicubic10_y.hlsl
-set LIST=%LIST% resizer_lanczos2_x.hlsl
-set LIST=%LIST% resizer_lanczos2_y.hlsl
-set LIST=%LIST% resizer_downscaling_x.hlsl
-set LIST=%LIST% resizer_downscaling_y.hlsl
-set LIST=%LIST% YCgCo_correction.hlsl
-set LIST=%LIST% halfoverunder_to_interlace.hlsl
+set LIST=%LIST% Resizers\resizer_bspline4_x.hlsl
+set LIST=%LIST% Resizers\resizer_bspline4_y.hlsl
+set LIST=%LIST% Resizers\resizer_mitchell4_x.hlsl
+set LIST=%LIST% Resizers\resizer_mitchell4_y.hlsl
+set LIST=%LIST% Resizers\resizer_catmull4_x.hlsl
+set LIST=%LIST% Resizers\resizer_catmull4_y.hlsl
+set LIST=%LIST% Resizers\resizer_bicubic06_x.hlsl
+set LIST=%LIST% Resizers\resizer_bicubic06_y.hlsl
+set LIST=%LIST% Resizers\resizer_bicubic08_x.hlsl
+set LIST=%LIST% Resizers\resizer_bicubic08_y.hlsl
+set LIST=%LIST% Resizers\resizer_bicubic10_x.hlsl
+set LIST=%LIST% Resizers\resizer_bicubic10_y.hlsl
+set LIST=%LIST% Resizers\resizer_lanczos2_x.hlsl
+set LIST=%LIST% Resizers\resizer_lanczos2_y.hlsl
+set LIST=%LIST% Resizers\resizer_downscaling_x.hlsl
+set LIST=%LIST% Resizers\resizer_downscaling_y.hlsl
+set LIST=%LIST% Transformation\YCgCo_correction.hlsl
+set LIST=%LIST% Transformation\halfoverunder_to_interlace.hlsl
 
 for %%f in (%LIST%) do (
   fxc.exe /nologo /T ps_2_0 /Fo ..\..\bin\shaders\ps20_%%~nf.cso %%f
 )
+
+pause
