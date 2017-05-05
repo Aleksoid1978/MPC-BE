9923d418b30a26cf5a0d87579ee91748c6f212ae
Aleksoid1978 <Aleksoid1978@mail.ru>
2017-05-05 05:04:41 +0000

    [r2536] Изменение : Добавлена поддержка открытия UHD Blu-Ray структуры, косметика кода.

--- src/DSUtil/HdmvClipInfo.h
+++ src/DSUtil/HdmvClipInfo.h
@@ -1,5 +1,5 @@
 /*
- * (C) 2006-2016 see Authors.txt
+ * (C) 2006-2017 see Authors.txt
  *
  * This file is part of MPC-BE.
  *
@@ -25,48 +25,49 @@
 #include "../../include/basestruct.h"
 
 enum BDVM_VideoFormat {
-	BDVM_VideoFormat_Unknown	= 0,
-	BDVM_VideoFormat_480i		= 1,
-	BDVM_VideoFormat_576i		= 2,
-	BDVM_VideoFormat_480p		= 3,
-	BDVM_VideoFormat_1080i		= 4,
-	BDVM_VideoFormat_720p		= 5,
-	BDVM_VideoFormat_1080p		= 6,
-	BDVM_VideoFormat_576p		= 7,
+	BDVM_VideoFormat_Unknown = 0,
+	BDVM_VideoFormat_480i    = 1,
+	BDVM_VideoFormat_576i    = 2,
+	BDVM_VideoFormat_480p    = 3,
+	BDVM_VideoFormat_1080i   = 4,
+	BDVM_VideoFormat_720p    = 5,
+	BDVM_VideoFormat_1080p   = 6,
+	BDVM_VideoFormat_576p    = 7,
+	BDVM_VideoFormat_2160p   = 8,
 };
 
 enum BDVM_FrameRate {
-	BDVM_FrameRate_Unknown		= 0,
-	BDVM_FrameRate_23_976		= 1,
-	BDVM_FrameRate_24			= 2,
-	BDVM_FrameRate_25			= 3,
-	BDVM_FrameRate_29_97		= 4,
-	BDVM_FrameRate_50			= 6,
-	BDVM_FrameRate_59_94		= 7
+	BDVM_FrameRate_Unknown = 0,
+	BDVM_FrameRate_23_976  = 1,
+	BDVM_FrameRate_24      = 2,
+	BDVM_FrameRate_25      = 3,
+	BDVM_FrameRate_29_97   = 4,
+	BDVM_FrameRate_50      = 6,
+	BDVM_FrameRate_59_94   = 7
 };
 
 enum BDVM_AspectRatio {
-	BDVM_AspectRatio_Unknown	= 0,
-	BDVM_AspectRatio_4_3		= 2,
-	BDVM_AspectRatio_16_9		= 3,
-	BDVM_AspectRatio_2_21		= 4
+	BDVM_AspectRatio_Unknown = 0,
+	BDVM_AspectRatio_4_3     = 2,
+	BDVM_AspectRatio_16_9    = 3,
+	BDVM_AspectRatio_2_21    = 4
 };
 
 enum BDVM_ChannelLayout {
-	BDVM_ChannelLayout_Unknown	= 0,
-	BDVM_ChannelLayout_MONO		= 1,
-	BDVM_ChannelLayout_STEREO	= 3,
-	BDVM_ChannelLayout_MULTI	= 6,
-	BDVM_ChannelLayout_COMBO	= 12
+	BDVM_ChannelLayout_Unknown = 0,
+	BDVM_ChannelLayout_MONO    = 1,
+	BDVM_ChannelLayout_STEREO  = 3,
+	BDVM_ChannelLayout_MULTI   = 6,
+	BDVM_ChannelLayout_COMBO   = 12
 };
 
 enum BDVM_SampleRate {
-	BDVM_SampleRate_Unknown		= 0,
-	BDVM_SampleRate_48			= 1,
-	BDVM_SampleRate_96			= 4,
-	BDVM_SampleRate_192			= 5,
-	BDVM_SampleRate_48_192		= 12,
-	BDVM_SampleRate_48_96		= 14
+	BDVM_SampleRate_Unknown = 0,
+	BDVM_SampleRate_48      = 1,
+	BDVM_SampleRate_96      = 4,
+	BDVM_SampleRate_192     = 5,
+	BDVM_SampleRate_48_192  = 12,
+	BDVM_SampleRate_48_96   = 14
 };
 
 class CHdmvClipInfo
@@ -74,24 +75,18 @@ class CHdmvClipInfo
 public:
 
 	struct Stream {
-		Stream() {
-			memset(this, 0, sizeof(*this));
-		}
-
-		SHORT					m_PID;
-		PES_STREAM_TYPE			m_Type;
-		char					m_LanguageCode[4];
-		LCID					m_LCID;
+		SHORT              m_PID  = 0;
+		PES_STREAM_TYPE    m_Type = INVALID;
+		char               m_LanguageCode[4] = {};
+		LCID               m_LCID = 0;
 
 		// Valid for video types
-		BDVM_VideoFormat		m_VideoFormat;
-		BDVM_FrameRate			m_FrameRate;
-		BDVM_AspectRatio		m_AspectRatio;
+		BDVM_VideoFormat   m_VideoFormat = BDVM_VideoFormat_Unknown;
+		BDVM_FrameRate     m_FrameRate   = BDVM_FrameRate_Unknown;
+		BDVM_AspectRatio   m_AspectRatio = BDVM_AspectRatio_Unknown;
 		// Valid for audio types
-		BDVM_ChannelLayout		m_ChannelLayout;
-		BDVM_SampleRate			m_SampleRate;
-
-		LPCTSTR Format();
+		BDVM_ChannelLayout m_ChannelLayout = BDVM_ChannelLayout_Unknown;
+		BDVM_SampleRate    m_SampleRate    = BDVM_SampleRate_Unknown;
 	};
 
 	struct PlaylistItem {
