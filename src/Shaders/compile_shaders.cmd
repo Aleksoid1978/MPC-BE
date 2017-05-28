cfec9e1a232e9c28383531e276bf8993e58c75dd
v0lt <mpc-be@mail.ru>
2017-05-28 05:48:27 +0000

    [r2587] Shaders: шейдер "Simple Average" ужат до одного файла.

--- src/Shaders/compile_shaders.cmd
+++ src/Shaders/compile_shaders.cmd
@@ -6,6 +6,8 @@ IF NOT EXIST %fxc% set fxc="c:\Program Files (x86)\Windows Kits\8.1\bin\x86\fxc.
 IF NOT EXIST %fxc% set fxc="C:\Program Files (x86)\Microsoft DirectX SDK (June 2010)\Utilities\bin\x86\fxc.exe"
 
 echo === Compiling downscaler shaders ===
+%fxc% /nologo /T ps_3_0 /Fo "..\..\bin\shaders\downscaler_simple_x.cso" "Resizers\downscaler_simple.hlsl" /DAXIS=0
+%fxc% /nologo /T ps_3_0 /Fo "..\..\bin\shaders\downscaler_simple_y.cso" "Resizers\downscaler_simple.hlsl" /DAXIS=1
 %fxc% /nologo /T ps_3_0 /Fo "..\..\bin\shaders\downscaler_box_x.cso" "Resizers\downscaler_box.hlsl" /DAXIS=0
 %fxc% /nologo /T ps_3_0 /Fo "..\..\bin\shaders\downscaler_box_y.cso" "Resizers\downscaler_box.hlsl" /DAXIS=1
 %fxc% /nologo /T ps_3_0 /Fo "..\..\bin\shaders\downscaler_bilinear_x.cso" "Resizers\downscaler.hlsl" /DFILTER=1 /DAXIS=0
@@ -32,8 +34,6 @@ set LIST=%LIST% Resizers\resizer_bicubic10_x.hlsl
 set LIST=%LIST% Resizers\resizer_bicubic10_y.hlsl
 set LIST=%LIST% Resizers\resizer_lanczos2_x.hlsl
 set LIST=%LIST% Resizers\resizer_lanczos2_y.hlsl
-set LIST=%LIST% Resizers\downscaler_simple_x.hlsl
-set LIST=%LIST% Resizers\downscaler_simple_y.hlsl
 set LIST=%LIST% Transformation\ycgco_correction.hlsl
 set LIST=%LIST% Transformation\st2084_correction.hlsl
 set LIST=%LIST% Transformation\hlg_correction.hlsl
@@ -43,5 +43,7 @@ for %%f in (%LIST%) do (
   %fxc% /nologo /T ps_2_0 /Fo ..\..\bin\shaders\ps20_%%~nf.cso %%f
 )
 
+%fxc% /nologo /T ps_2_0 /Fo "..\..\bin\shaders\ps20_downscaler_simple_x.cso" "Resizers\downscaler_simple.hlsl" /DPS20 /DAXIS=0
+%fxc% /nologo /T ps_2_0 /Fo "..\..\bin\shaders\ps20_downscaler_simple_y.cso" "Resizers\downscaler_simple.hlsl" /DPS20 /DAXIS=1
 %fxc% /nologo /T ps_2_0 /Fo "..\..\bin\shaders\ps20_downscaler_box_x.cso" "Resizers\downscaler_box.hlsl" /DPS20 /DAXIS=0
 %fxc% /nologo /T ps_2_0 /Fo "..\..\bin\shaders\ps20_downscaler_box_y.cso" "Resizers\downscaler_box.hlsl" /DPS20 /DAXIS=1
