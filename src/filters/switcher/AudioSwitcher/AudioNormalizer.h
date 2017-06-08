030ccc414a30fb9577ffdba45952709a6536f3c5
Aleksoid1978 <Aleksoid1978@mail.ru>
2017-06-08 06:20:08 +0000

    [r2626] Изменение : "Почищен и причесан" код CAudioNormalizer.

--- src/filters/switcher/AudioSwitcher/AudioNormalizer.h
+++ src/filters/switcher/AudioSwitcher/AudioNormalizer.h
@@ -1,5 +1,5 @@
 /*
- * (C) 2014-2016 see Authors.txt
+ * (C) 2014-2017 see Authors.txt
  *
  * This file is part of MPC-BE.
  *
@@ -22,8 +22,6 @@
 
 #include <atlcoll.h>
 
-#define AUTOVOLUME 0
-
 class CAudioNormalizer
 {
 protected:
@@ -31,64 +29,21 @@ protected:
 	bool m_boost;
 	int m_stepping;
 	int m_rising;
-	int m_drangehead;
-	int m_vol_level[64];
-	int m_vol_head;
-
-	CAtlArray<short> m_bufLQ;
-	CAtlArray<int> m_smpLQ;
 
 	CAtlArray<double> m_bufHQ;
 	CAtlArray<double> m_smpHQ;
-	double m_drange[64];
-	double m_SMP;
 
-	BYTE m_prediction[4096];
 	DWORD m_predictor;
-	DWORD m_pred_good;
-	DWORD m_pred_bad;
+	BYTE m_prediction[4096];
+
 	int m_vol;
 
-#if AUTOVOLUME
-	struct smooth_t {
-		double * data;
-		double max;
-		int size;
-		int used;
-		int current;
-	};
-
-	smooth_t *m_smooth[10];
-	double m_normalize_level;
-	double m_silence_level;
-	double m_max_mult;
-	bool m_do_compress;
-	double m_cutoff;
-	double m_degree;
-
-	void calc_power_level(short *samples, int numsamples, int nch);
-	void adjust_gain(short *samples, int numsamples, int nch, double gain);
-	smooth_t *SmoothNew(int size);
-	void SmoothDelete(smooth_t * del);
-	void SmoothAddSample(smooth_t * sm, double sample);
-	double SmoothGetMax(smooth_t * sm);
-#endif
-
-	int MSteadyLQ(void *samples, int numsamples, int nch, bool IsFloat);
-	int MSteadyHQ(void *samples, int numsamples, int nch, bool IsFloat);
-
-public:
-	void SetParam(int Level, bool Boost, int Steping);
-	int MSteadyLQ16(short *samples, int numsamples, int nch);
-	int MSteadyLQ32(float *samples, int numsamples, int nch);
-	int MSteadyHQ16(short *samples, int numsamples, int nch);
-	int MSteadyHQ32(float *samples, int numsamples, int nch);
-
-#if AUTOVOLUME
-	int AutoVolume(short *samples, int numsamples, int nch);
-#endif
+	int ProcessInternal(float *samples, unsigned numsamples, unsigned nch);
 
 public:
 	CAudioNormalizer(void);
 	virtual ~CAudioNormalizer(void);
+
+	void SetParam(int Level, bool Boost, int Steping);
+	int Process(float *samples, unsigned numsamples, unsigned nch);
 };
