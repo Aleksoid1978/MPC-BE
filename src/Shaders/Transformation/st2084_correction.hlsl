647b8da3a18a8597c399bea47d8a1370eff7f56e
aleksoid <aleksoid@7426f7ed-705a-47fc-8cda-d59144bb0dce>
2017-06-25 06:29:49 +0000

    Изменение : Улучшено HDR -> SDR преобразование, добавлен код для преобразования Rec.2020 -> sRGB/Rec.709 gamut(использован код библиотеки zimg - https://github.com/sekrit-twc/zimg). Косметика.
    git-svn-id: svn://svn.code.sf.net/p/mpcbe/code/trunk@2645 7426f7ed-705a-47fc-8cda-d59144bb0dce

--- src/Shaders/Transformation/st2084_correction.hlsl
+++ src/Shaders/Transformation/st2084_correction.hlsl
@@ -8,10 +8,11 @@ sampler image : register(s0);
 #define ST2084_c2 ((2413.0 / 4096.0) * 32.0)
 #define ST2084_c3 ((2392.0 / 4096.0) * 32.0)
 
-#define SRC_LUMINANCE_PEAK     7500.0
-#define DISPLAY_LUMINANCE_PEAK 150.0
+#define SRC_LUMINANCE_PEAK     10000.0
+#define DISPLAY_LUMINANCE_PEAK 125.0
 
 #include "hdr_tone_mapping.hlsl"
+#include "colorspace_gamut_conversion.hlsl"
 
 #pragma warning( disable : 3571) // fix warning X3571 in pow().
 
@@ -25,11 +26,14 @@ float4 main(float2 tex : TEXCOORD0) : COLOR
     pixel.rgb = max(pixel.rgb - ST2084_c1, 0.0) / (ST2084_c2 - ST2084_c3 * pixel.rgb);
     pixel.rgb = pow(pixel.rgb, 1.0 / ST2084_m1);
 
+    // Peak luminance
+    pixel.rgb = pixel.rgb * (SRC_LUMINANCE_PEAK / DISPLAY_LUMINANCE_PEAK);
+
     // HDR tone mapping
     pixel.rgb = ToneMappingHable(pixel.rgb);
 
-    // Peak luminance
-    pixel.rgb = pixel.rgb * (SRC_LUMINANCE_PEAK / DISPLAY_LUMINANCE_PEAK);
+    // Colorspace Gamut Conversion
+    pixel.rgb = Colorspace_Gamut_Conversion_2020_to_709(pixel.rgb);
 
     // Linear to sRGB
     pixel.rgb = saturate(pixel.rgb);
