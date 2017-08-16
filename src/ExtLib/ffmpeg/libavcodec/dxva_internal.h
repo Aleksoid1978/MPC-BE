e3cbf96b377b9e7a694a2af61c16795de960486e
Aleksoid1978 <Aleksoid1978@mail.ru>
2017-08-16 23:27:12 +0000

    [r2819] Изменение : ffmpeg/libavcodec/dxva_internal.h - добавлены недостающие #include

--- src/ExtLib/ffmpeg/libavcodec/dxva_internal.h
+++ src/ExtLib/ffmpeg/libavcodec/dxva_internal.h
@@ -18,6 +18,8 @@
  *
  */
 
+#include <stdint.h>
+#include <d3d9.h>
 #include <dxva2api.h>
 
 #define FF_DXVA2_WORKAROUND_SCALING_LIST_ZIGZAG 1 ///< Work around for DXVA2 and old UVD/UVD+ ATI video cards
