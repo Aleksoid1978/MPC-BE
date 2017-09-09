5fe275d03dd07fd1cefe496ab4aa4af2a7e6b898
v0lt <mpc-be@mail.ru>
2017-09-09 11:03:14 +0000

    [r2927] MPCVideoDec/MSDKDecoder: резервируем значение для режима вывода.

--- src/filters/transform/MPCVideoDec/MSDKDecoder/MSDKDecoder.h
+++ src/filters/transform/MPCVideoDec/MSDKDecoder/MSDKDecoder.h
@@ -52,8 +52,9 @@ typedef struct _MVCGOP {
 
 enum {
   MVC_OUTPUT_Auto = 0,
-  MVC_OUTPUT_Mono,     // force mono frame (left)
-  MVC_OUTPUT_TopBottom // convert to stereoscopic half height anamorphic video
+  MVC_OUTPUT_Mono,          // force mono frame (left)
+  MVC_OUTPUT_HalfTopBottom, // convert to stereoscopic half height anamorphic video
+  MVC_OUTPUT_TopBottom      // convert to two images packed vertically (reserved)
 };
 
 class CMPCVideoDecFilter;
