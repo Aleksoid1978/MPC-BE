ca4997fc90c6d77c5bf0fcd6b612d07819e392b4
v0lt <mpc-be@mail.ru>
2017-10-13 18:03:16 +0000

    [r2996] DSUtil: Рефакторинг и переименование функции IsLikelyPath -> IsLikelyFilePath. Перед вызовом SHAddToRecentDocs(SHARD_PATHW, filename) всегда проверяем правильность имени файла, т.к. с URL могут быть проблемы.

--- src/DSUtil/DSUtil.cpp
+++ src/DSUtil/DSUtil.cpp
@@ -1481,17 +1481,18 @@ CString MakeFullPath(LPCTSTR path)
 	return CString(c);
 }
 
-bool IsLikelyPath(LPCTSTR str) // stupid path detector
+
+bool IsLikelyFilePath(const CString& str) // simple file system path detector
 {
-	if (_tcslen(str) > 4) {
-		// local path
+	if (str.GetLength() >= 4) {
+		// local file path
 		if (str[1] == ':' && str[2] == '\\' &&
 				(str[0] >= 'A' && str[0] <= 'Z' || str[0] >= 'a' && str[0] <= 'z')) {
 			return true;
 		}
 
-		// net path
-		if (str[0] == '\\' && str[1] == '\\' &&
+		// net file path
+		if (str.GetLength() >= 7 && str[0] == '\\' && str[1] == '\\' &&
 				(str[2] == '-' || str[2] >= '0' && str[2] <= '9' ||  str[2] >= 'A' && str[2] <= 'Z' || str[2] >= 'a' && str[2] <= 'z')) {
 			return true;
 		}
