7ef18f0fb835d673c8c36123a5952a53a240a226
v0lt <v0lt@7426f7ed-705a-47fc-8cda-d59144bb0dce>
2017-11-08 15:29:44 +0000

    DSUtil: используем функции std::min/max вместо макросов min/max, доработка кода.
    git-svn-id: svn://svn.code.sf.net/p/mpcbe/code/trunk@3080 7426f7ed-705a-47fc-8cda-d59144bb0dce

--- src/DSUtil/GolombBuffer.cpp
+++ src/DSUtil/GolombBuffer.cpp
@@ -145,7 +145,7 @@ void CGolombBuffer::ReadBuffer(BYTE* pDest, int nSize)
 {
 	ASSERT(m_nBitPos + nSize <= m_nSize);
 	ASSERT(m_bitlen == 0);
-	nSize = min (nSize, m_nSize - m_nBitPos);
+	nSize = std::min(nSize, m_nSize - m_nBitPos);
 
 	memcpy(pDest, m_pBuffer + m_nBitPos, nSize);
 	m_nBitPos += nSize;
