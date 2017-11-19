471e57b29964e6b36e90c8ad2a94dbac5637c0ed
Aleksoid1978 <Aleksoid1978@mail.ru>
2017-11-19 13:05:01 +0000

    [r3136] Изменение : Корректировка r3130.

--- src/DSUtil/DXVAState.h
+++ src/DSUtil/DXVAState.h
@@ -23,50 +23,9 @@
 #include <Windows.h>
 
 namespace DXVAState {
-	static BOOL m_bDXVActive      = FALSE;
-	static GUID m_guidDXVADecoder = GUID_NULL;
-
-	static CString m_sDXVADecoderDescription = L"Software";
-	static CString m_sDXVADecoderShortDescription;
-
-	inline void ClearState()
-	{
-		m_bDXVActive      = FALSE;
-		m_guidDXVADecoder = GUID_NULL;
-
-		m_sDXVADecoderDescription      = L"Software";
-		m_sDXVADecoderShortDescription = L"";
-	}
-
-	inline void SetActiveState(const GUID& guidDXVADecoder, const CString& customDescription = L"")
-	{
-		m_bDXVActive = TRUE;
-
-		if (!customDescription.IsEmpty()) {
-			m_sDXVADecoderDescription = customDescription;
-			if (m_guidDXVADecoder != GUID_NULL) {
-				m_sDXVADecoderDescription.AppendFormat(L", %s", GetDXVAMode(m_guidDXVADecoder));
-			}
-			m_sDXVADecoderShortDescription = L"H/W";
-		} else if (guidDXVADecoder != GUID_NULL) {
-			m_guidDXVADecoder = guidDXVADecoder;
-			m_sDXVADecoderShortDescription = L"DXVA2";
-			m_sDXVADecoderDescription.Format(L"DXVA2 Native, %s", GetDXVAMode(m_guidDXVADecoder));
-		}
-	}
-
-	inline const BOOL GetState()
-	{
-		return m_bDXVActive;
-	}
-
-	inline const CString GetDescription()
-	{
-		return m_sDXVADecoderDescription;
-	}
-
-	inline const CString GetShortDescription()
-	{
-		return m_sDXVADecoderShortDescription;
-	}
+	void ClearState();
+	void SetActiveState(const GUID& guidDXVADecoder, const CString& customDescription = L"");
+	const BOOL GetState();
+	const CString GetDescription();
+	const CString GetShortDescription();
 }
