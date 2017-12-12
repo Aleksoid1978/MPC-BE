d5d53245f99a80dd8f3193cb3192a2fa4e8625eb
v0lt <mpc-be@mail.ru>
2017-12-12 16:23:58 +0000

    [r3184] Шейдеры: откорректирован шейдер grayscale.hlsl, добавлен шейдер treshold.hlsl.

--- distrib/Shaders/grayscale.hlsl
+++ distrib/Shaders/grayscale.hlsl
@@ -1,9 +1,8 @@
 // $MinimumShaderProfile: ps_2_0
-
 sampler s0 : register(s0);
 
 float4 main(float2 tex : TEXCOORD0) : COLOR {
-	float c0 = dot(tex2D(s0, tex), float4(0.299, 0.587, 0.114, 0));
+	float c0 = dot(tex2D(s0, tex), float4(0.2126, 0.7152, 0.0722, 0));
 
 	return c0;
 }
