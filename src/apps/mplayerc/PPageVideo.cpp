211289d637ac9cb60d33eabc1707b6dff9e0a436
v0lt <v0lt@7426f7ed-705a-47fc-8cda-d59144bb0dce>
2018-01-30 18:25:21 +0000

    Убраны "*" для методов уменьшения картинки.
    git-svn-id: svn://svn.code.sf.net/p/mpcbe/code/trunk@3343 7426f7ed-705a-47fc-8cda-d59144bb0dce

--- src/apps/mplayerc/PPageVideo.cpp
+++ src/apps/mplayerc/PPageVideo.cpp
@@ -349,11 +349,11 @@ void CPPageVideo::UpdateDownscalerList(int select)
 	}
 #endif
 	AddStringData(m_cbDownscaler, L"PS: Simple averaging", DOWNSCALER_SIMPLE);
-	AddStringData(m_cbDownscaler, L"PS: Box*", DOWNSCALER_BOX);
-	AddStringData(m_cbDownscaler, L"PS 3.0: Bilinear*", DOWNSCALER_BILINEAR);
-	AddStringData(m_cbDownscaler, L"PS 3.0: Hamming*", DOWNSCALER_HAMMING);
-	AddStringData(m_cbDownscaler, L"PS 3.0: Bicubic*", DOWNSCALER_BICUBIC);
-	AddStringData(m_cbDownscaler, L"PS 3.0: Lanczos*", DOWNSCALER_LANCZOS);
+	AddStringData(m_cbDownscaler, L"PS: Box", DOWNSCALER_BOX);
+	AddStringData(m_cbDownscaler, L"PS 3.0: Bilinear", DOWNSCALER_BILINEAR);
+	AddStringData(m_cbDownscaler, L"PS 3.0: Hamming", DOWNSCALER_HAMMING);
+	AddStringData(m_cbDownscaler, L"PS 3.0: Bicubic", DOWNSCALER_BICUBIC);
+	AddStringData(m_cbDownscaler, L"PS 3.0: Lanczos", DOWNSCALER_LANCZOS);
 
 	m_cbDownscaler.SetCurSel(1); // default
 	SelectByItemData(m_cbDownscaler, select);
