532e78033b12f30c690ec319f474bece0482257c
v0lt <v0lt@7426f7ed-705a-47fc-8cda-d59144bb0dce>
2018-02-08 03:10:09 +0000

    Subtitles/SubtitleHelpers: файлы с расширением .txt более не рассматриваются как файлы субтитрами.
    git-svn-id: svn://svn.code.sf.net/p/mpcbe/code/trunk@3367 7426f7ed-705a-47fc-8cda-d59144bb0dce

--- src/Subtitles/SubtitleHelpers.h
+++ src/Subtitles/SubtitleHelpers.h
@@ -30,7 +30,7 @@ namespace Subtitle
 	static const LPCWSTR s_SubFileExts[] = {
 		L"srt", L"ass", L"ssa", L"sub", L"idx",
 		L"smi", L"sup", L"usf", L"vtt", L"psb",
-		L"xss", L"txt", L"rt",  L"mks", L"lrc"
+		L"xss", L"rt",  L"mks", L"lrc"
 	};
 
 	enum SubType {
