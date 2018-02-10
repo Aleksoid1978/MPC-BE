255d44ed7f0740f2bc2edebfebef732aa506d5e8
v0lt <mpc-be@mail.ru>
2018-02-10 07:28:11 +0000

    [r3375] Убраны лишние #include <atlcoll.h>.

--- src/apps/mplayerc/MediaTypesDlg.h
+++ src/apps/mplayerc/MediaTypesDlg.h
@@ -1,6 +1,6 @@
 /*
  * (C) 2003-2006 Gabest
- * (C) 2006-2017 see Authors.txt
+ * (C) 2006-2018 see Authors.txt
  *
  * This file is part of MPC-BE.
  *
@@ -22,7 +22,6 @@
 #pragma once
 
 #include <afxwin.h>
-#include <atlcoll.h>
 #include "BaseGraph.h"
 #include <ResizableLib/ResizableDialog.h>
 
--- src/apps/mplayerc/PlayerInfoBar.h
+++ src/apps/mplayerc/PlayerInfoBar.h
@@ -1,6 +1,6 @@
 /*
  * (C) 2003-2006 Gabest
- * (C) 2006-2016 see Authors.txt
+ * (C) 2006-2018 see Authors.txt
  *
  * This file is part of MPC-BE.
  *
@@ -21,7 +21,6 @@
 
 #pragma once
 
-#include <atlcoll.h>
 #include "StatusLabel.h"
 
 class CMainFrame;
