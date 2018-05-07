cb606f9073639d75c366e04b2d5409c3f57fcf19
Aleksoid1978 <Aleksoid1978@mail.ru>
2018-05-07 05:46:31 +0000

    [r3601] Изменение : HdmvSub - косметика кода.

--- src/Subtitles/BaseSub.h
+++ src/Subtitles/BaseSub.h
@@ -1,5 +1,5 @@
 /*
- * (C) 2006-2017 see Authors.txt
+ * (C) 2006-2018 see Authors.txt
  *
  * This file is part of MPC-BE.
  *
@@ -40,7 +40,7 @@ public:
 	virtual HRESULT			Render(SubPicDesc& spd, REFERENCE_TIME rt, RECT& bbox) PURE;
 	virtual HRESULT			GetTextureSize (POSITION pos, SIZE& MaxTextureSize, SIZE& VideoSize, POINT& VideoTopLeft) PURE;
 	virtual void			CleanOld(REFERENCE_TIME rt) PURE;
-	virtual HRESULT			EndOfStream() PURE;
+	virtual HRESULT			EndOfStream() { return S_OK; }
 
 	HRESULT					SetConvertType(CString _yuvMatrix, ColorConvert::convertType _convertType) { yuvMatrix = _yuvMatrix; convertType = _convertType; return S_OK; };
 
