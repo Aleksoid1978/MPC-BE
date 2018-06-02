4614c19c1df820995ea3168f4c59c86aeb0a21ee
v0lt <mpc-be@mail.ru>
2018-06-02 13:34:04 +0000

    [r3692] Прячем неработающий "Surround level"

--- src/apps/mplayerc/PPageSoundProcessing.cpp
+++ src/apps/mplayerc/PPageSoundProcessing.cpp
@@ -151,6 +151,10 @@ BOOL CPPageSoundProcessing::OnInitDialog()
 	m_sldCenter.SetPos((int)std::round(s.fAudioCenter_dB * 10));
 	m_sldSurround.SetRange(APP_AUDIOLEVEL_MIN * 10, APP_AUDIOLEVEL_MAX * 10, TRUE);
 	m_sldSurround.SetPos((int)std::round(s.fAudioSurround_dB * 10));
+#ifndef _DEBUG
+	m_stcSurround.ShowWindow(SW_HIDE);
+	m_sldSurround.ShowWindow(SW_HIDE);
+#endif
 
 	m_sldGain.SetRange(APP_AUDIOGAIN_MIN * 10, APP_AUDIOGAIN_MAX * 10, TRUE);
 	m_sldGain.SetPos((int)std::round(s.dAudioGain_dB * 10));
