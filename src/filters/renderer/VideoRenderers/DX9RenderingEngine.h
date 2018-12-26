f810d059a177c176b38e4c28868a08b11ccee46f
Aleksoid1978 <Aleksoid1978@mail.ru>
2018-12-26 23:44:05 +0000

    [r4289] Изменение : Корректировка/исправление r4288.

--- src/filters/renderer/VideoRenderers/DX9RenderingEngine.h
+++ src/filters/renderer/VideoRenderers/DX9RenderingEngine.h
@@ -197,11 +197,11 @@ namespace DSObjects
 		// Resizers
 		HRESULT InitShaderResizer(int resizer);
 	protected:
-		HRESULT TextureResize(IDirect3DTexture9* pTexture, const CRect& srcRect, const CRect& destRect, D3DTEXTUREFILTERTYPE filter);
+		HRESULT Resize(IDirect3DTexture9* pTexture, const CRect& srcRect, const CRect& destRect);
 	private:
+		HRESULT TextureResize(IDirect3DTexture9* pTexture, const CRect& srcRect, const CRect& destRect, D3DTEXTUREFILTERTYPE filter);
 		HRESULT TextureResizeShader(IDirect3DTexture9* pTexture, const CRect& srcRect, const CRect& destRect, int iShader);
 		HRESULT ApplyResize(IDirect3DTexture9* pTexture, const CRect& srcRect, const CRect& destRect, int resizer, int y);
-		HRESULT Resize(IDirect3DTexture9* pTexture, const CRect& srcRect, const CRect& destRect);
 
 		// Final pass
 		HRESULT InitFinalPass();
