e5aec3bbccf8445871f65976b85f22434560a27f
aleksoid <aleksoid@7426f7ed-705a-47fc-8cda-d59144bb0dce>
2019-02-22 00:55:23 +0000

    Изменение : MpegSplitter - корректировка перемотки MPEG-PS.
    git-svn-id: svn://svn.code.sf.net/p/mpcbe/code/trunk@4440 7426f7ed-705a-47fc-8cda-d59144bb0dce

--- src/filters/parser/MpegSplitter/MpegSplitterFile.cpp
+++ src/filters/parser/MpegSplitter/MpegSplitterFile.cpp
@@ -489,10 +489,7 @@ REFERENCE_TIME CMpegSplitterFile::NextPTS(const DWORD TrackNum, const stream_cod
 				__int64 pos3 = GetPos();
 				nextPos = pos3 + h.len;
 
-				stream s;
-				s.pid = 0;
-				s.pesid = b;
-				if (h.fpts && s == TrackNum && AddStream(0, b, h.id_ext, h.len, FALSE) == TrackNum) {
+				if (h.fpts && b == (TrackNum >> 8) && AddStream(0, b, h.id_ext, h.len, FALSE) == TrackNum) {
 					if (rtLimit != _I64_MAX && (h.pts - rtMin) > rtLimit) {
 						Seek(pos);
 						return INVALID_TIME;
