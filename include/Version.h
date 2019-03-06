5f6a2e3d95e7e11f1922328a7fd47938058c28d8
aleksoid <aleksoid@7426f7ed-705a-47fc-8cda-d59144bb0dce>
2019-03-06 04:53:30 +0000

    Изменение : Обновлен год 2018 -> 2019.
    git-svn-id: svn://svn.code.sf.net/p/mpcbe/code/trunk@4456 7426f7ed-705a-47fc-8cda-d59144bb0dce

--- include/Version.h
+++ include/Version.h
@@ -41,7 +41,7 @@
 // MPC_VERSION_STATUS: 0 - beta; 1 - stable
 
 #define MPC_WND_CLASS_NAME      "MPC-BE"
-#define MPC_YEAR_COMMENTS       "2002-2018"
+#define MPC_YEAR_COMMENTS       "2002-2019"
 #define MPC_VERSION_COMMENTS    "http://sourceforge.net/projects/mpcbe/"
 
 #ifndef ISPP_INVOKED
@@ -49,7 +49,7 @@
 #define MPC_WND_CLASS_NAMEW     _CRT_WIDE(MPC_WND_CLASS_NAME)
 
 #define MPC_COMP_NAME_STR       L"MPC-BE Team"
-#define MPC_COPYRIGHT_STR       L"Copyright © 2002-2018 all contributors, see Authors.txt"
+#define MPC_COPYRIGHT_STR       L"Copyright © 2002-2019 all contributors, see Authors.txt"
 
 #define MPC_VERSION_NUM         MPC_VERSION_MAJOR,MPC_VERSION_MINOR,MPC_VERSION_PATCH
 #define MPC_VERSION_STR         MAKE_STR(MPC_VERSION_MAJOR) "." MAKE_STR(MPC_VERSION_MINOR) "." MAKE_STR(MPC_VERSION_PATCH)
