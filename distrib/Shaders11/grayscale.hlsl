26723b8a2ab0acc60b631ad0dac156a70b27998c
v0lt <v0lt@7426f7ed-705a-47fc-8cda-d59144bb0dce>
2020-01-19 05:39:27 +0000

    Shaders11: добавлен grayscale.hlsl, invert.hlsl. Косметика.
    git-svn-id: svn://svn.code.sf.net/p/mpcbe/code/trunk@5019 7426f7ed-705a-47fc-8cda-d59144bb0dce

--- distrib/Shaders11/grayscale.hlsl
+++ distrib/Shaders11/grayscale.hlsl
@@ -5,7 +5,7 @@ SamplerState samp : register(s0);
 
 float4 main(float4 pos : SV_POSITION, float2 coord : TEXCOORD) : SV_Target
 {
-	float c = dot(tex.Sample(samp, coord), float4(0.2, 0.6, 0.1, 0.1));
+	float c = dot(tex.Sample(samp, coord), float4(0.2126, 0.7152, 0.0722, 0));
 
-	return float4(0, c, 0, 0);
+	return c;
 }
