2d15e08ec9a54e173da7e3f4d690d49e212fec20
v0lt <mpc-be@mail.ru>
2020-01-19 05:39:27 +0000

    [r5019] Shaders11: добавлен grayscale.hlsl, invert.hlsl. Косметика.

--- distrib/Shaders11/invert.hlsl
+++ distrib/Shaders11/invert.hlsl
@@ -5,7 +5,7 @@ SamplerState samp : register(s0);
 
 float4 main(float4 pos : SV_POSITION, float2 coord : TEXCOORD) : SV_Target
 {
-	float c = dot(tex.Sample(samp, coord), float4(0.2, 0.6, 0.1, 0.1));
+	float4 c = float4(1, 1, 1, 1) - tex.Sample(samp, coord);
 
-	return float4(0, c, 0, 0);
+	return c;
 }
--- distrib/Shaders11/wave.hlsl
+++ distrib/Shaders11/wave.hlsl
@@ -8,18 +8,18 @@ cbuffer PS_CONSTANTS : register(b0)
 	float2 wh;
 	uint   counter;
 	float  clock;
-	float2 dxdy;
+	float2 dxy;
 };
 
-float4 main(float4 Pos : SV_POSITION, float2 Tex : TEXCOORD) : SV_Target
+float4 main(float4 pos : SV_POSITION, float2 coord : TEXCOORD) : SV_Target
 {
 	float4 c0 = 0;
 
-	Tex.x += sin(Tex.x + clock / 0.3) / 20;
-	Tex.y += sin(Tex.x + clock / 0.3) / 20;
+	coord.x += sin(coord.x + clock / 0.3) / 20;
+	coord.y += sin(coord.x + clock / 0.3) / 20;
 
-	if (Tex.x >= 0 && Tex.x <= 1 && Tex.y >= 0 && Tex.y <= 1) {
-		c0 = tex.Sample(samp, Tex);
+	if (coord.x >= 0 && coord.x <= 1 && coord.y >= 0 && coord.y <= 1) {
+		c0 = tex.Sample(samp, coord);
 	}
 
 	return c0;
