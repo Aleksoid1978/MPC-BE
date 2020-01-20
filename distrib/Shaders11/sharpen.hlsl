6b3630ae0f8064b35dcacbac1387635698999769
v0lt <mpc-be@mail.ru>
2020-01-20 11:22:35 +0000

    [r5026] Shaders11: добавлены Adaptive sharpen.hlsl и sharpen.hlsl.

---
+++ distrib/Shaders11/sharpen.hlsl
@@ -0,0 +1,37 @@
+// $MinimumShaderProfile: ps_4_0
+
+Texture2D tex : register(t0);
+SamplerState samp : register(s0);
+
+cbuffer PS_CONSTANTS : register(b0)
+{
+	float2 pxy;
+	float  width;
+	float  height;
+	uint   counter;
+	float  clock;
+};
+
+#define val0 (2.0)
+#define val1 (-0.125)
+#define effect_width (1.6)
+
+float4 main(float4 pos : SV_POSITION, float2 coord : TEXCOORD) : SV_Target
+{
+	float dx = effect_width / width;
+	float dy = effect_width / height;
+
+	float4 c1 = tex.Sample(samp, coord + float2(-dx, -dy)) * val1;
+	float4 c2 = tex.Sample(samp, coord + float2(  0, -dy)) * val1;
+	float4 c3 = tex.Sample(samp, coord + float2(-dx,   0)) * val1;
+	float4 c4 = tex.Sample(samp, coord + float2( dx,   0)) * val1;
+	float4 c5 = tex.Sample(samp, coord + float2(  0,  dy)) * val1;
+	float4 c6 = tex.Sample(samp, coord + float2( dx,  dy)) * val1;
+	float4 c7 = tex.Sample(samp, coord + float2(-dx, +dy)) * val1;
+	float4 c8 = tex.Sample(samp, coord + float2(+dx, -dy)) * val1;
+	float4 c9 = tex.Sample(samp, coord) * val0;
+
+	float4 c0 = (c1 + c2 + c3 + c4 + c5 + c6 + c7 + c8 + c9);
+
+	return c0;
+}
