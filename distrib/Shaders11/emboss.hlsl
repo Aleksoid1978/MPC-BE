67dce33ee30f7e377596ff9737e393174e90c018
v0lt <v0lt@7426f7ed-705a-47fc-8cda-d59144bb0dce>
2020-01-20 10:44:24 +0000

    Синхронизация кода шейдеров DX11 c MPC VR v0.4.0.1104.
    git-svn-id: svn://svn.code.sf.net/p/mpcbe/code/trunk@5025 7426f7ed-705a-47fc-8cda-d59144bb0dce

--- distrib/Shaders11/emboss.hlsl
+++ distrib/Shaders11/emboss.hlsl
@@ -5,20 +5,21 @@ SamplerState samp : register(s0);
 
 cbuffer PS_CONSTANTS : register(b0)
 {
+	float  px;
+	float  py;
 	float2 wh;
 	uint   counter;
 	float  clock;
-	float2 dxy;
 };
 
 float4 main(float4 pos : SV_POSITION, float2 coord : TEXCOORD) : SV_Target
 {
-	float4 c1 = tex.Sample(samp, coord + float2(-dxy.x, -dxy.y));
-	float4 c2 = tex.Sample(samp, coord + float2(     0, -dxy.y));
-	float4 c4 = tex.Sample(samp, coord + float2(-dxy.x,      0));
-	float4 c6 = tex.Sample(samp, coord + float2( dxy.x,      0));
-	float4 c8 = tex.Sample(samp, coord + float2(     0,  dxy.y));
-	float4 c9 = tex.Sample(samp, coord + float2( dxy.x,  dxy.y));
+	float4 c1 = tex.Sample(samp, coord + float2(-px, -py));
+	float4 c2 = tex.Sample(samp, coord + float2(  0, -py));
+	float4 c4 = tex.Sample(samp, coord + float2(-px,   0));
+	float4 c6 = tex.Sample(samp, coord + float2( px,   0));
+	float4 c8 = tex.Sample(samp, coord + float2(  0,  py));
+	float4 c9 = tex.Sample(samp, coord + float2( px,  py));
 
 	float4 c0 = (-c1 - c2 - c4 + c6 + c8 + c9);
 	c0 = (c0.r + c0.g + c0.b) / 3 + 0.5;
