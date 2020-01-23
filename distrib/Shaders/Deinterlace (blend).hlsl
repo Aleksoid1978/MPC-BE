74e2137b93e15557a5ce8b308e14736dc3affef8
v0lt <mpc-be@mail.ru>
2020-01-23 07:07:01 +0000

    [r5039] Переименование файлов внешних шейдеров, косметика кода. Добавлены варианты для DX11.

--- distrib/Shaders/Deinterlace (blend).hlsl
+++ distrib/Shaders/Deinterlace (blend).hlsl
@@ -1,12 +1,14 @@
 // $MinimumShaderProfile: ps_2_0
 
+// Run this shader before scaling.
+
 sampler s0 : register(s0);
 float4 p0 :  register(c0);
 
-#define width  (p0[0])
 #define height (p0[1])
 
-float4 main(float2 tex : TEXCOORD0) : COLOR {
+float4 main(float2 tex : TEXCOORD0) : COLOR
+{
 	float4 c0 = tex2D(s0, tex);
 
 	float2 h = float2(0, 1 / height);
