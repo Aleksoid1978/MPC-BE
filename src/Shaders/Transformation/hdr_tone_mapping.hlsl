afbb8f85d2ba41d71dc395f98162b7eb447f7ea1
v0lt <mpc-be@mail.ru>
2020-04-23 05:23:20 +0000

    [r5228] Shaders: убрана лишняя функция.

--- src/Shaders/Transformation/hdr_tone_mapping.hlsl
+++ src/Shaders/Transformation/hdr_tone_mapping.hlsl
@@ -1,22 +1,13 @@
-const float hable(const float x)
+inline float3 hable(float3 x)
 {
     const float A = 0.15, B = 0.50, C = 0.10, D = 0.20, E = 0.02, F = 0.30;
 
     return ((x * (A * x + (C * B)) + (D * E)) / (x * (A * x + B) + (D * F))) - E / F;
 }
 
-const float3 hable(float3 rgb)
-{
-    rgb.r = hable(rgb.r);
-    rgb.g = hable(rgb.g);
-    rgb.b = hable(rgb.b);
-
-    return rgb;
-}
-
 float3 ToneMappingHable(const float3 rgb)
 {
-    static const float HABLE_DIV = hable(4.8);
+    static const float3 HABLE_DIV = hable(4.8);
 
     return hable(rgb) / HABLE_DIV;
 }
