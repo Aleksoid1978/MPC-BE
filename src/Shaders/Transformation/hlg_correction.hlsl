662eca0d4e6f286f0837142403a4ade75841f364
v0lt <v0lt@7426f7ed-705a-47fc-8cda-d59144bb0dce>
2020-04-27 03:25:55 +0000

    Shaders: упрощение функции inverse_HLG.
    git-svn-id: svn://svn.code.sf.net/p/mpcbe/code/trunk@5230 7426f7ed-705a-47fc-8cda-d59144bb0dce

--- src/Shaders/Transformation/hlg_correction.hlsl
+++ src/Shaders/Transformation/hlg_correction.hlsl
@@ -7,16 +7,15 @@ sampler image : register(s0);
 
 #include "hdr_tone_mapping.hlsl"
 
-inline float inverse_HLG(float x)
+inline float3 inverse_HLG(float3 x)
 {
     const float B67_a = 0.17883277;
     const float B67_b = 0.28466892;
     const float B67_c = 0.55991073;
     const float B67_inv_r2 = 4.0;
-    if (x <= 0.5)
-        x = x * x * B67_inv_r2;
-    else
-        x = exp((x - B67_c) / B67_a) + B67_b;
+    x = (x <= 0.5)
+        ? x * x * B67_inv_r2
+        : exp((x - B67_c) / B67_a) + B67_b;
 
     return x;
 }
@@ -26,9 +25,7 @@ float4 main(float2 tex : TEXCOORD0) : COLOR
     float4 pixel = tex2D(image, tex);
 
     // HLG to Linear
-    pixel.r = inverse_HLG(pixel.r);
-    pixel.g = inverse_HLG(pixel.g);
-    pixel.b = inverse_HLG(pixel.b);
+    pixel.rgb = inverse_HLG(pixel.rgb);
     pixel.rgb /= 12.0;
 
     // HDR tone mapping
