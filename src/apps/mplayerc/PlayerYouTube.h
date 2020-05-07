550162a00e2bb4576a09278243fff47ee5a3cf26
v0lt <v0lt@7426f7ed-705a-47fc-8cda-d59144bb0dce>
2020-05-07 17:35:05 +0000

    Переработка r5277. Косметика кода.
    git-svn-id: svn://svn.code.sf.net/p/mpcbe/code/trunk@5278 7426f7ed-705a-47fc-8cda-d59144bb0dce

--- src/apps/mplayerc/PlayerYouTube.h
+++ src/apps/mplayerc/PlayerYouTube.h
@@ -26,13 +26,10 @@
 namespace Youtube
 {
 	enum yformat {
-		y_unknown,
-		y_mp4,
+		y_mp4 = 0,
 		y_webm,
 		y_mp4_av1,
-		// not used
-		y_flv,
-		y_3gp,
+		// flv, 3gp are not used.
 	};
 
 	enum ytype {
@@ -54,59 +51,60 @@ namespace Youtube
 
 	// MP4, WebM and 360p resolution or above only
 	static const YoutubeProfile YProfiles[] = {
+		//iTag| format |   type |quality| ext |  fps60 | hdr |live
 		// MP4 (H.264)
-		{266, y_mp4,  y_video, 2160, L"mp4",  false, false},
-		{138, y_mp4,  y_video, 2160, L"mp4",  false, false},
-		{264, y_mp4,  y_video, 1440, L"mp4",  false, false},
-		{299, y_mp4,  y_video, 1080, L"mp4",  true , false},
-		{137, y_mp4,  y_video, 1080, L"mp4",  false, false},
-		{298, y_mp4,  y_video,  720, L"mp4",  true , false},
-		{ 22, y_mp4,  y_media,  720, L"mp4",  false, false}, // H.264 + AAC
-		{136, y_mp4,  y_video,  720, L"mp4",  false, false},
-		{135, y_mp4,  y_video,  480, L"mp4",  false, false},
-		{ 18, y_mp4,  y_media,  360, L"mp4",  false, false}, // H.264 + AAC
-		{134, y_mp4,  y_video,  360, L"mp4",  false, false},
-		{133, y_mp4,  y_video,  240, L"mp4",  false, false},
+		{266, y_mp4,     y_video, 2160, L"mp4",  false, false},
+		{138, y_mp4,     y_video, 2160, L"mp4",  false, false},
+		{264, y_mp4,     y_video, 1440, L"mp4",  false, false},
+		{299, y_mp4,     y_video, 1080, L"mp4",  true , false},
+		{137, y_mp4,     y_video, 1080, L"mp4",  false, false},
+		{298, y_mp4,     y_video,  720, L"mp4",  true , false},
+		{ 22, y_mp4,     y_media,  720, L"mp4",  false, false}, // H.264 + AAC
+		{136, y_mp4,     y_video,  720, L"mp4",  false, false},
+		{135, y_mp4,     y_video,  480, L"mp4",  false, false},
+		{ 18, y_mp4,     y_media,  360, L"mp4",  false, false}, // H.264 + AAC
+		{134, y_mp4,     y_video,  360, L"mp4",  false, false},
+		{133, y_mp4,     y_video,  240, L"mp4",  false, false},
 		// WebM (VP9)
-		{272, y_webm, y_video, 4320, L"webm", true, false},
-		{272, y_webm, y_video, 4320, L"webm", false, false},
-		{272, y_webm, y_video, 2880, L"webm", false, false},
-		{337, y_webm, y_video, 2160, L"webm", true , true },
-		{315, y_webm, y_video, 2160, L"webm", true , false},
-		{313, y_webm, y_video, 2160, L"webm", false, false},
-		{336, y_webm, y_video, 1440, L"webm", true , true },
-		{308, y_webm, y_video, 1440, L"webm", true , false},
-		{271, y_webm, y_video, 1440, L"webm", false, false},
-		{335, y_webm, y_video, 1080, L"webm", true , true },
-		{303, y_webm, y_video, 1080, L"webm", true , false},
-		{248, y_webm, y_video, 1080, L"webm", false, false},
-		{334, y_webm, y_video,  720, L"webm", true , true },
-		{302, y_webm, y_video,  720, L"webm", true , false},
-		{247, y_webm, y_video,  720, L"webm", false, false},
-		{244, y_webm, y_video,  480, L"webm", false, false},
-		{ 43, y_webm, y_media,  360, L"webm", false, false}, // VP8 + Vorbis
-		{243, y_webm, y_video,  360, L"webm", false, false},
-		{242, y_webm, y_video,  240, L"webm", false, false},
+		{272, y_webm,    y_video, 4320, L"webm", true,  false},
+		{272, y_webm,    y_video, 4320, L"webm", false, false},
+		{272, y_webm,    y_video, 2880, L"webm", false, false},
+		{337, y_webm,    y_video, 2160, L"webm", true , true },
+		{315, y_webm,    y_video, 2160, L"webm", true , false},
+		{313, y_webm,    y_video, 2160, L"webm", false, false},
+		{336, y_webm,    y_video, 1440, L"webm", true , true },
+		{308, y_webm,    y_video, 1440, L"webm", true , false},
+		{271, y_webm,    y_video, 1440, L"webm", false, false},
+		{335, y_webm,    y_video, 1080, L"webm", true , true },
+		{303, y_webm,    y_video, 1080, L"webm", true , false},
+		{248, y_webm,    y_video, 1080, L"webm", false, false},
+		{334, y_webm,    y_video,  720, L"webm", true , true },
+		{302, y_webm,    y_video,  720, L"webm", true , false},
+		{247, y_webm,    y_video,  720, L"webm", false, false},
+		{244, y_webm,    y_video,  480, L"webm", false, false},
+		{ 43, y_webm,    y_media,  360, L"webm", false, false}, // VP8 + Vorbis
+		{243, y_webm,    y_video,  360, L"webm", false, false},
+		{242, y_webm,    y_video,  240, L"webm", false, false},
 		// MP4 (AV1)
-		{571, y_mp4_av1,  y_video, 4320, L"mp4",  false, false},
-		{402, y_mp4_av1,  y_video, 4320, L"mp4",  false, false},
-		{401, y_mp4_av1,  y_video, 2160, L"mp4",  false, false},
-		{400, y_mp4_av1,  y_video, 1440, L"mp4",  false, false},
-		{399, y_mp4_av1,  y_video, 1080, L"mp4",  false, false},
-		{398, y_mp4_av1,  y_video,  720, L"mp4",  false, false},
-		{397, y_mp4_av1,  y_video,  480, L"mp4",  false, false},
-		{396, y_mp4_av1,  y_video,  360, L"mp4",  false, false},
-		{395, y_mp4_av1,  y_video,  240, L"mp4",  false, false},
+		{571, y_mp4_av1, y_video, 4320, L"mp4",  false, false},
+		{402, y_mp4_av1, y_video, 4320, L"mp4",  false, false},
+		{401, y_mp4_av1, y_video, 2160, L"mp4",  false, false},
+		{400, y_mp4_av1, y_video, 1440, L"mp4",  false, false},
+		{399, y_mp4_av1, y_video, 1080, L"mp4",  false, false},
+		{398, y_mp4_av1, y_video,  720, L"mp4",  false, false},
+		{397, y_mp4_av1, y_video,  480, L"mp4",  false, false},
+		{396, y_mp4_av1, y_video,  360, L"mp4",  false, false},
+		{395, y_mp4_av1, y_video,  240, L"mp4",  false, false},
 		// live (ts)
-		{267, y_mp4,  y_media, 2160, L"ts",   false, false, true},
-		{265, y_mp4,  y_media, 1440, L"ts",   false, false, true},
-		{301, y_mp4,  y_media, 1080, L"ts",   true , false, true},
-		{300, y_mp4,  y_media,  720, L"ts",   true , false, true},
-		{ 96, y_mp4,  y_media, 1080, L"ts",   false, false, true},
-		{ 95, y_mp4,  y_media,  720, L"ts",   false, false, true},
-		{ 94, y_mp4,  y_media,  480, L"ts",   false, false, true},
-		{ 93, y_mp4,  y_media,  360, L"ts",   false, false, true},
-		{ 92, y_mp4,  y_media,  240, L"ts",   false, false, true},
+		{267, y_mp4,     y_media, 2160, L"ts",   false, false, true},
+		{265, y_mp4,     y_media, 1440, L"ts",   false, false, true},
+		{301, y_mp4,     y_media, 1080, L"ts",   true , false, true},
+		{300, y_mp4,     y_media,  720, L"ts",   true , false, true},
+		{ 96, y_mp4,     y_media, 1080, L"ts",   false, false, true},
+		{ 95, y_mp4,     y_media,  720, L"ts",   false, false, true},
+		{ 94, y_mp4,     y_media,  480, L"ts",   false, false, true},
+		{ 93, y_mp4,     y_media,  360, L"ts",   false, false, true},
+		{ 92, y_mp4,     y_media,  240, L"ts",   false, false, true},
 	};
 
 	static const YoutubeProfile YAudioProfiles[] = {
