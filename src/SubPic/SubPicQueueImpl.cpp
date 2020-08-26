287643cfb53cf0fbd0c91cb94a98f9360e1e1182
Aleksoid1978 <Aleksoid1978@mail.ru>
2020-08-26 12:48:25 +0000

    [r5436] Рендеринг субтитров - исправлено мерцание некоторых субтитров(спасибо команде MPC-HC).

--- src/SubPic/SubPicQueueImpl.cpp
+++ src/SubPic/SubPicQueueImpl.cpp
@@ -549,7 +549,7 @@ DWORD CSubPicQueue::ThreadProc()
 				}
 
 				REFERENCE_TIME rtCurrent = std::max(rtStart, rtStartRendering);
-				if (rtCurrent > m_rtNow) {
+				if (rtCurrent > m_rtNow && rtTimePerFrame <= rtStop - rtStart) {
 					// Round current time to the next estimated video frame timing
 					REFERENCE_TIME rtCurrentRounded = (rtCurrent / rtTimePerFrame) * rtTimePerFrame;
 					if (rtCurrentRounded < rtCurrent) {
