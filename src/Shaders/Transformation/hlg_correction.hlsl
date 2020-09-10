60ecc2378c48a735f69dc6d53976100fcffb5256
Aleksoid1978 <Aleksoid1978@mail.ru>
2020-09-10 01:15:07 +0000

    [r5463] Корректировка шейдера hlg_correction, спасибо VictorLS и Iron_Butterfly за наводку.

--- src/Shaders/Transformation/hlg_correction.hlsl
+++ src/Shaders/Transformation/hlg_correction.hlsl
@@ -6,6 +6,7 @@ sampler image : register(s0);
 #define DISPLAY_LUMINANCE_PEAK 80.0
 
 #include "hdr_tone_mapping.hlsl"
+#include "colorspace_gamut_conversion.hlsl"
 
 inline float3 inverse_HLG(float3 x)
 {
@@ -30,6 +31,9 @@ float4 main(float2 tex : TEXCOORD0) : COLOR
 
     // HDR tone mapping
     pixel.rgb = ToneMappingHable(pixel.rgb);
+
+    // Colorspace Gamut Conversion
+    pixel.rgb = Colorspace_Gamut_Conversion_2020_to_709(pixel.rgb);
 
     // Peak luminance
     pixel.rgb = pixel.rgb * (SRC_LUMINANCE_PEAK / DISPLAY_LUMINANCE_PEAK);
