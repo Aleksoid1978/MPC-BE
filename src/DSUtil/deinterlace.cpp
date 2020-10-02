a6f1f441b42c6a7c62038324924d0bac62288bd8
v0lt <mpc-be@mail.ru>
2020-10-02 19:25:20 +0000

    [r5553] DSUtil: Т.к. поддержка SSE2 обязательна, то убраны более ненужные проверки и лишний код в коде BlendPlane.

--- src/DSUtil/deinterlace.cpp
+++ src/DSUtil/deinterlace.cpp
@@ -20,7 +20,6 @@
 
 #include <emmintrin.h>
 #include <vd2/system/memory.h>
-#include <vd2/system/cpuaccel.h>
 #include <vd2/system/vdstl.h>
 
 #pragma warning(disable: 4799) // warning C4799: function has no EMMS instruction
@@ -61,106 +60,6 @@ xloop:
 
 		mov		[edi+esi-4],eax
 		jnz		xloop
-
-		pop		ebx
-		pop		esi
-		pop		edi
-		pop		ebp
-		ret
-	};
-}
-
-static void __declspec(naked) asm_blend_row(void *dst, const void *src, uint32 w, ptrdiff_t srcpitch) {
-	__asm {
-		push	ebp
-		push	edi
-		push	esi
-		push	ebx
-
-		mov		edi,[esp+20]
-		mov		esi,[esp+24]
-		sub		edi,esi
-		mov		ebp,[esp+28]
-		mov		edx,[esp+32]
-
-xloop:
-		mov		ecx,[esi]
-		mov		eax,0fcfcfcfch
-
-		mov		ebx,[esi+edx]
-		and		eax,ecx
-
-		shr		ebx,1
-		mov		ecx,[esi+edx*2]
-
-		shr		ecx,2
-		and		ebx,07f7f7f7fh
-
-		shr		eax,2
-		and		ecx,03f3f3f3fh
-
-		add		eax,ebx
-		add		esi,4
-
-		add		eax,ecx
-		dec		ebp
-
-		mov		[edi+esi-4],eax
-		jnz		xloop
-
-		pop		ebx
-		pop		esi
-		pop		edi
-		pop		ebp
-		ret
-	};
-}
-
-static void __declspec(naked) asm_blend_row_MMX(void *dst, const void *src, uint32 w, ptrdiff_t srcpitch) {
-	static const __declspec(align(8)) __int64 mask0 = 0xfcfcfcfcfcfcfcfci64;
-	static const __declspec(align(8)) __int64 mask1 = 0x7f7f7f7f7f7f7f7fi64;
-	static const __declspec(align(8)) __int64 mask2 = 0x3f3f3f3f3f3f3f3fi64;
-	__asm {
-		push	ebp
-		push	edi
-		push	esi
-		push	ebx
-
-		mov		edi,[esp+20]
-		mov		esi,[esp+24]
-		sub		edi,esi
-		mov		ebp,[esp+28]
-		mov		edx,[esp+32]
-
-		movq	mm5,mask0
-		movq	mm6,mask1
-		movq	mm7,mask2
-		inc		ebp
-		shr		ebp,1
-xloop:
-		movq	mm2,[esi]
-		movq	mm0,mm5
-
-		movq	mm1,[esi+edx]
-		pand	mm0,mm2
-
-		psrlq	mm1,1
-		movq	mm2,[esi+edx*2]
-
-		psrlq	mm2,2
-		pand	mm1,mm6
-
-		psrlq	mm0,2
-		pand	mm2,mm7
-
-		paddb	mm0,mm1
-		add		esi,8
-
-		paddb	mm0,mm2
-		dec		ebp
-
-		movq	[edi+esi-8],mm0
-		jne		xloop
 
 		pop		ebx
 		pop		esi
@@ -224,23 +123,6 @@ static void asm_blend_row_clipped(void *dst0, const void *src0, uint32 w, ptrdif
 		*dst++ = (x|y) - (((x^y)&0xfefefefe)>>1);
 	} while(--w);
 }
-
-static void asm_blend_row(void *dst0, const void *src0, uint32 w, ptrdiff_t srcpitch) {
-	uint32 *dst = (uint32 *)dst0;
-	const uint32 *src = (const uint32 *)src0;
-	const uint32 *src2 = (const uint32 *)((const char *)src + srcpitch);
-	const uint32 *src3 = (const uint32 *)((const char *)src2 + srcpitch);
-
-	do {
-		const uint32 a = *src++;
-		const uint32 b = *src2++;
-		const uint32 c = *src3++;
-		const uint32 hi = (a & 0xfcfcfc) + 2*(b & 0xfcfcfc) + (c & 0xfcfcfc);
-		const uint32 lo = (a & 0x030303) + 2*(b & 0x030303) + (c & 0x030303) + 0x020202;
-
-		*dst++ = (hi + (lo & 0x0c0c0c))>>2;
-	} while(--w);
-}
 #endif
 
 #if defined(VD_CPU_X86) || defined(VD_CPU_AMD64)
@@ -267,10 +149,10 @@ static void asm_blend_row_SSE2(void *dst, const void *src, uint32 w, ptrdiff_t s
 void BlendPlane(void *dst, ptrdiff_t dstpitch, const void *src, ptrdiff_t srcpitch, uint32 w, uint32 h) {
 	void (*blend_func)(void *, const void *, uint32, ptrdiff_t);
 #if defined(VD_CPU_X86)
-	if (SSE2_enabled && !(srcpitch % 16))
+	if (!(srcpitch % 16))
 		blend_func = asm_blend_row_SSE2;
 	else
-		blend_func = ISSE_enabled ? asm_blend_row_ISSE : MMX_enabled ? asm_blend_row_MMX : asm_blend_row;
+		blend_func = asm_blend_row_ISSE;
 #else
 	blend_func = asm_blend_row_SSE2;
 #endif
@@ -290,8 +172,7 @@ void BlendPlane(void *dst, ptrdiff_t dstpitch, const void *src, ptrdiff_t srcpit
 	asm_blend_row_clipped((char *)dst + dstpitch, src, w, srcpitch);
 
 #ifdef _M_IX86
-	if (MMX_enabled)
-		__asm emms
+	__asm emms
 #endif
 }
 
