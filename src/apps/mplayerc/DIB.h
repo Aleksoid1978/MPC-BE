833d38c08f1dc134727280df1e77e5c8c20969c3
v0lt <v0lt@7426f7ed-705a-47fc-8cda-d59144bb0dce>
2020-10-07 16:38:46 +0000

    Корректировка функций PNGDIB и WICDIB.
    git-svn-id: svn://svn.code.sf.net/p/mpcbe/code/trunk@5570 7426f7ed-705a-47fc-8cda-d59144bb0dce

--- src/apps/mplayerc/DIB.h
+++ src/apps/mplayerc/DIB.h
@@ -26,17 +26,19 @@
 static bool PNGDIB(LPCWSTR filename, BYTE* pData, int level)
 {
 	BITMAPINFOHEADER* bih = (BITMAPINFOHEADER*)pData;
-	if (bih->biCompression != BI_RGB || bih->biWidth <= 0 || abs(bih->biHeight) == 0 || bih->biBitCount % 8) {
+	if (bih->biCompression != BI_RGB
+			|| bih->biWidth <= 0 || abs(bih->biHeight) == 0
+			|| (bih->biBitCount != 32 && bih->biBitCount != 64)) {
 		return false;
 	}
 
 	FILE* fp;
 	if (_wfopen_s(&fp, filename, L"wb") == 0) {
-		const unsigned width = bih->biWidth;
-		const unsigned height = abs(bih->biHeight);
-		const int bit_depth = (bih->biBitCount == 48 || bih->biBitCount == 64) ? 16 : 8; // bits per channel
-		const unsigned src_bpp = bih->biBitCount / 8;
-		const unsigned dst_bpp = 3 * bit_depth / 8;
+		const unsigned width   = bih->biWidth;
+		const unsigned height  = abs(bih->biHeight);
+		const int bit_depth    = bih->biBitCount / 4; // bits per channel
+		const unsigned src_bpp = bih->biBitCount / 8; // bytes per pixel
+		const unsigned dst_bpp = 3 * bit_depth / 8;   // bytes per pixel
 
 		png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, 0, 0, 0);
 		png_infop info_ptr = png_create_info_struct(png_ptr);
@@ -46,38 +48,40 @@ static bool PNGDIB(LPCWSTR filename, BYTE* pData, int level)
 		png_set_IHDR(png_ptr, info_ptr, width, height, bit_depth, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE, 0, 0);
 		png_write_info(png_ptr, info_ptr);
 
-		BYTE* row_ptr = (png_bytep)malloc(width * dst_bpp);
-		const unsigned src_pitch = width * src_bpp;
-		BYTE* src = pData + sizeof(BITMAPINFOHEADER) + src_pitch * (height - 1);
+		BYTE* row_ptr = new(std::nothrow) BYTE[width * dst_bpp];
+		if (row_ptr) {
+			const unsigned src_pitch = width * src_bpp;
+			BYTE* src = pData + sizeof(BITMAPINFOHEADER) + src_pitch * (height - 1);
 
-		for (unsigned y = 0; y < height; ++y) {
-			if (bit_depth == 16) {
-				uint16_t* src16 = (uint16_t*)src;
-				uint16_t* dst16 = (uint16_t*)row_ptr;
-				for (unsigned x = 0; x < width; ++x) {
-					*dst16++ = _byteswap_ushort(src16[2]);
-					*dst16++ = _byteswap_ushort(src16[1]);
-					*dst16++ = _byteswap_ushort(src16[0]);
-					src16 += 4;
+			for (unsigned y = 0; y < height; ++y) {
+				if (bit_depth == 16) {
+					uint16_t* src16 = (uint16_t*)src;
+					uint16_t* dst16 = (uint16_t*)row_ptr;
+					for (unsigned x = 0; x < width; ++x) {
+						*dst16++ = _byteswap_ushort(src16[2]);
+						*dst16++ = _byteswap_ushort(src16[1]);
+						*dst16++ = _byteswap_ushort(src16[0]);
+						src16 += 4;
+					}
 				}
-			}
-			else {
-				uint8_t* src8 = (uint8_t*)src;
-				uint8_t* dst8 = (uint8_t*)row_ptr;
-				for (unsigned x = 0; x < width; ++x) {
-					*dst8++ = src8[2];
-					*dst8++ = src8[1];
-					*dst8++ = src8[0];
-					src8 += 4;
+				else {
+					uint8_t* src8 = (uint8_t*)src;
+					uint8_t* dst8 = (uint8_t*)row_ptr;
+					for (unsigned x = 0; x < width; ++x) {
+						*dst8++ = src8[2];
+						*dst8++ = src8[1];
+						*dst8++ = src8[0];
+						src8 += 4;
+					}
 				}
+				png_write_row(png_ptr, row_ptr);
+				src -= src_pitch;
 			}
-			png_write_row(png_ptr, row_ptr);
-			src -= src_pitch;
+			delete[] row_ptr;
 		}
-		png_write_end(png_ptr, info_ptr);
 
+		png_write_end(png_ptr, info_ptr);
 		fclose(fp);
-		free(row_ptr);
 		png_destroy_write_struct(&png_ptr, &info_ptr);
 
 		return true;
@@ -89,8 +93,19 @@ static bool PNGDIB(LPCWSTR filename, BYTE* pData, int level)
 static bool WICDIB(LPCWSTR filename, BYTE* pData, int quality, BYTE* output, size_t& outLen)
 {
 	BITMAPINFOHEADER* bih = (BITMAPINFOHEADER*)pData;
+	if (bih->biCompression != BI_RGB || bih->biWidth <= 0 || abs(bih->biHeight) == 0) {
+		return false;
+	}
 
-	const WICPixelFormatGUID format = (bih->biBitCount == 32) ? GUID_WICPixelFormat32bppBGR : GUID_WICPixelFormat24bppBGR;
+	WICPixelFormatGUID format = {};
+
+	switch (bih->biBitCount) {
+	case 24: format = GUID_WICPixelFormat24bppBGR; break;
+	case 32: format = GUID_WICPixelFormat32bppBGR; break;
+	default:
+		return false;
+	}
+
 	const UINT pitch = bih->biWidth * bih->biBitCount / 8;
 	BYTE* src = pData + sizeof(BITMAPINFOHEADER) + bih->biClrUsed * 4;
 
