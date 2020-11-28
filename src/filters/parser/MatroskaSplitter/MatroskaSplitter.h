8f177bec6c1ee984aaab83f090c895c6e041954b
v0lt <v0lt@7426f7ed-705a-47fc-8cda-d59144bb0dce>
2020-11-28 12:09:42 +0000

    MatroskaSplitter: Замена QWORD на UINT64.
    git-svn-id: svn://svn.code.sf.net/p/mpcbe/code/trunk@5748 7426f7ed-705a-47fc-8cda-d59144bb0dce

--- src/filters/parser/MatroskaSplitter/MatroskaSplitter.h
+++ src/filters/parser/MatroskaSplitter/MatroskaSplitter.h
@@ -1,6 +1,6 @@
 /*
  * (C) 2003-2006 Gabest
- * (C) 2006-2019 see Authors.txt
+ * (C) 2006-2020 see Authors.txt
  *
  * This file is part of MPC-BE.
  *
@@ -69,7 +69,7 @@ class __declspec(uuid("149D2E01-C32E-4939-80F6-C07B81015A7A"))
 	CAutoPtr<CMatroskaNode> m_pSegment, m_pCluster, m_pBlock;
 
 	REFERENCE_TIME m_Cluster_seek_rt;
-	QWORD m_Cluster_seek_pos;
+	UINT64 m_Cluster_seek_pos;
 	BOOL m_bSupportSubtitlesCueDuration;
 	std::vector<UINT64> m_subtitlesTrackNumbers;
 
