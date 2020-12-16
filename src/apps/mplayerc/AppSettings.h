e2fa0f6f3776cb1cbd0716d951b1fa919c51b0d8
v0lt <mpc-be@mail.ru>
2020-12-16 16:48:30 +0000

    [r5819] Устранены древние и очень странные небольшие утечки памяти. Спасибо Paul Mitchell. Решение - https://stackoverflow.com/a/13529855

--- src/apps/mplayerc/AppSettings.h
+++ src/apps/mplayerc/AppSettings.h
@@ -229,7 +229,7 @@ enum : int {
 	PLAYBACKWND_FITSCREENLARGER,
 };
 
-inline const std::vector<int> s_CommonVideoHeights = { 240, 360, 480, 720, 1080, 1440, 2160, 2880, 4320 };
+static const std::vector<int> s_CommonVideoHeights = { 240, 360, 480, 720, 1080, 1440, 2160, 2880, 4320 };
 
 struct ShaderC {
 	CString   label;
