81421ed41e0b3cc5ddd03c9d91c7849e880734b0
aleksoid <aleksoid@7426f7ed-705a-47fc-8cda-d59144bb0dce>
2020-12-18 02:29:42 +0000

    EVR-CP - убрана обработка VideoTransferFunction == VIDEOTRANSFUNC_HLG, но добавлена обработка VideoPrimaries == VIDEOPRIMARIES_BT2020.
    git-svn-id: svn://svn.code.sf.net/p/mpcbe/code/trunk@5823 7426f7ed-705a-47fc-8cda-d59144bb0dce

--- src/Shaders/Transformation/convert_bt2020_to_bt709.hlsl
+++ src/Shaders/Transformation/convert_bt2020_to_bt709.hlsl
@@ -1,4 +1,4 @@
-// Fix incorrect display ARIB STD-B67 (HLG) after incorrect YUV to RGB conversion in EVR Mixer.
+// Convert VideoPrimaries BT.2020 to BT.709
 
 sampler image : register(s0);
 
@@ -8,8 +8,16 @@ float4 main(float2 tex : TEXCOORD0) : COLOR
 {
     float4 pixel = tex2D(image, tex);
 
+    // sRGB to to Linear
+    pixel = saturate(pixel);
+    pixel = pow(pixel, 2.2);
+
     // Colorspace Gamut Conversion
     pixel.rgb = Colorspace_Gamut_Conversion_2020_to_709(pixel.rgb);
+
+    // Linear to sRGB
+    pixel = saturate(pixel);
+    pixel = pow(pixel, 1.0 / 2.2);
 
     return pixel;
 }
