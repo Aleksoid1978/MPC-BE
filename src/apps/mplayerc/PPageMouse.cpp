ae8dc3085f3230b0b1713ba892e04a47ebb228f6
v0lt <v0lt@7426f7ed-705a-47fc-8cda-d59144bb0dce>
2020-12-29 10:28:27 +0000

    Косметическая корректировка опции "Дополнительно добавить в плейлист". Косметика ресурсных файлов.
    git-svn-id: svn://svn.code.sf.net/p/mpcbe/code/trunk@5879 7426f7ed-705a-47fc-8cda-d59144bb0dce

--- src/apps/mplayerc/PPageMouse.cpp
+++ src/apps/mplayerc/PPageMouse.cpp
@@ -54,8 +54,8 @@ CPPageMouse::CPPageMouse()
 	m_comands_W.Add(ID_PLAY_SEEKBACKWARDMED,     IDS_AG_JUMP_BACKWARD_2);
 	m_comands_W.Add(ID_NAVIGATE_SKIPFORWARDFILE, IDS_AG_NEXT_FILE);
 	m_comands_W.Add(ID_NAVIGATE_SKIPBACKFILE,    IDS_AG_PREVIOUS_FILE);
-	m_comands_W.Add(ID_SUB_DELAY_UP,             IDS_MPLAYERC_105);
-	m_comands_W.Add(ID_SUB_DELAY_DOWN,           IDS_MPLAYERC_104);
+	m_comands_W.Add(ID_SUB_DELAY_INC,            IDS_AG_SUBDELAY_INC);
+	m_comands_W.Add(ID_SUB_DELAY_DEC,            IDS_AG_SUBDELAY_DEC);
 }
 
 CPPageMouse::~CPPageMouse()
