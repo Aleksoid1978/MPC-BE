d1d12ee6ef5437946a9aa5f5966f2f2f9d095474
v0lt <mpc-be@mail.ru>
2022-07-31 08:25:48 +0300

    Убран префикс "g" в начале хеша Git. Он не нужен (т.к. приписка "git" пишется ранее) и создает неудобства при поиске коммитов.

--- update_revision.cmd
+++ update_revision.cmd
@@ -22,8 +22,9 @@ FOR /F "tokens=2 delims=-" %%A IN ("%GIT_DESCRIBE_STR%") DO (
   SET GIT_REV_COUNT=%%A
 )
 FOR /F "tokens=3 delims=-" %%A IN ("%GIT_DESCRIBE_STR%") DO (
-  SET GIT_REV_HASH="%%A"
+  SET GIT_REV_HASH=%%A
 )
+IF %GIT_REV_HASH:~0,1%==g SET GIT_REV_HASH=%GIT_REV_HASH:~1%
 
 SET SrcManifest="src\apps\mplayerc\res\mpc-be.exe.manifest.conf"
 SET DstManifest="src\apps\mplayerc\res\mpc-be.exe.manifest"
@@ -31,12 +32,12 @@ SET DstManifest="src\apps\mplayerc\res\mpc-be.exe.manifest"
 IF NOT EXIST "revision.h" GOTO :UPDATE_REV
 IF NOT EXIST %DstManifest% GOTO :UPDATE_REV
 
-SET REVHASH=0
+SET REVHASH="0"
 SET REVNUM=0
-FOR /F "tokens=3,4 delims= " %%A IN ('FINDSTR /I /L /C:"define REV_HASH" "revision.h"') DO SET "REVHASH=%%A"
-FOR /F "tokens=3,4 delims= " %%A IN ('FINDSTR /I /L /C:"define REV_NUM" "revision.h"') DO SET "REVNUM=%%A"
+FOR /F "tokens=3,4 delims= " %%A IN ('FINDSTR /I /L /C:"define REV_HASH" "revision.h"') DO SET REVHASH=%%A
+FOR /F "tokens=3,4 delims= " %%A IN ('FINDSTR /I /L /C:"define REV_NUM" "revision.h"') DO SET REVNUM=%%A
 
-IF NOT %REVHASH%==%GIT_REV_HASH% GOTO :UPDATE_REV
+IF NOT %REVHASH%=="%GIT_REV_HASH%" GOTO :UPDATE_REV
 IF NOT %REVNUM%==%GIT_REV_COUNT% GOTO :UPDATE_REV
 
 GOTO :DoNotUpdate
@@ -52,7 +53,7 @@ SET GIT_REV_BRANCH=LOCAL
 FOR /F "delims=" %%A IN ('%gitexe% symbolic-ref --short HEAD') DO SET GIT_REV_BRANCH=%%A
 ECHO #define REV_BRANCH "%GIT_REV_BRANCH%" >> revision.h
 
-ECHO #define REV_HASH %GIT_REV_HASH% >> revision.h
+ECHO #define REV_HASH "%GIT_REV_HASH%" >> revision.h
 ECHO #define REV_NUM %GIT_REV_COUNT% >> revision.h
 
 IF EXIST %dstfile% DEL /Q %DstManifest%
