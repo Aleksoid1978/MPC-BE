46512d72767649a85726a9102660536d1a032e6a
Aleksoid <Aleksoid1978@mail.ru>
2024-01-13 11:51:14 +1000

    Корректировка кода, убраны неиспользуемые переменные.

--- src/DSUtil/VideoParser.cpp
+++ src/DSUtil/VideoParser.cpp
@@ -1643,8 +1643,6 @@ namespace VVCParser {
 	#define VVC_MAX_DPB_SIZE 16
 	#define VVC_MAX_REF_ENTRIES (VVC_MAX_DPB_SIZE + 13)
 	#define VVC_MAX_CPB_CNT 32
-	#define VVC_MAX_TILE_COLUMNS 20
-	#define VVC_MAX_TILE_ROWS 440
 	#define VVC_MAX_WIDTH 16888
 	#define VVC_MAX_HEIGHT 16888
 
@@ -2043,91 +2041,6 @@ namespace VVCParser {
 		uint16_t pps_conf_win_right_offset;
 		uint16_t pps_conf_win_top_offset;
 		uint16_t pps_conf_win_bottom_offset;
-
-		uint8_t  pps_scaling_window_explicit_signalling_flag;
-		int16_t  pps_scaling_win_left_offset;
-		int16_t  pps_scaling_win_right_offset;
-		int16_t  pps_scaling_win_top_offset;
-		int16_t  pps_scaling_win_bottom_offset;
-
-		uint8_t  pps_output_flag_present_flag;
-		uint8_t  pps_no_pic_partition_flag;
-
-		uint8_t  pps_subpic_id_mapping_present_flag;
-		uint16_t pps_num_subpics_minus1;
-		uint8_t  pps_subpic_id_len_minus1;
-		uint16_t pps_subpic_id[VVC_MAX_SLICES];
-
-		uint8_t  pps_log2_ctu_size_minus5;
-		uint8_t  pps_num_exp_tile_columns_minus1;
-		uint8_t  pps_num_exp_tile_rows_minus1;
-		uint16_t pps_tile_column_width_minus1[VVC_MAX_TILE_COLUMNS];
-		uint16_t pps_tile_row_height_minus1[VVC_MAX_TILE_ROWS];
-
-		uint8_t  pps_loop_filter_across_tiles_enabled_flag;
-		uint8_t  pps_rect_slice_flag;
-		uint8_t  pps_single_slice_per_subpic_flag;
-
-		uint16_t pps_num_slices_in_pic_minus1;
-		uint8_t  pps_tile_idx_delta_present_flag;
-		uint16_t pps_slice_width_in_tiles_minus1[VVC_MAX_SLICES];
-		uint16_t pps_slice_height_in_tiles_minus1[VVC_MAX_SLICES];
-		uint16_t pps_num_exp_slices_in_tile[VVC_MAX_SLICES];
-		uint16_t pps_exp_slice_height_in_ctus_minus1[VVC_MAX_SLICES][VVC_MAX_TILE_ROWS];
-		int16_t  pps_tile_idx_delta_val[VVC_MAX_SLICES];
-
-		uint8_t  pps_loop_filter_across_slices_enabled_flag;
-		uint8_t  pps_cabac_init_present_flag;
-		uint8_t  pps_num_ref_idx_default_active_minus1[2];
-		uint8_t  pps_rpl1_idx_present_flag;
-		uint8_t  pps_weighted_pred_flag;
-		uint8_t  pps_weighted_bipred_flag;
-		uint8_t  pps_ref_wraparound_enabled_flag;
-		uint16_t pps_pic_width_minus_wraparound_offset;
-		int8_t   pps_init_qp_minus26;
-		uint8_t  pps_cu_qp_delta_enabled_flag;
-		uint8_t  pps_chroma_tool_offsets_present_flag;
-		int8_t   pps_cb_qp_offset;
-		int8_t   pps_cr_qp_offset;
-		uint8_t  pps_joint_cbcr_qp_offset_present_flag;
-		int8_t   pps_joint_cbcr_qp_offset_value;
-		uint8_t  pps_slice_chroma_qp_offsets_present_flag;
-		uint8_t  pps_cu_chroma_qp_offset_list_enabled_flag;
-		uint8_t  pps_chroma_qp_offset_list_len_minus1;
-		int8_t   pps_cb_qp_offset_list[6];
-		int8_t   pps_cr_qp_offset_list[6];
-		int8_t   pps_joint_cbcr_qp_offset_list[6];
-		uint8_t  pps_deblocking_filter_control_present_flag;
-		uint8_t  pps_deblocking_filter_override_enabled_flag;
-		uint8_t  pps_deblocking_filter_disabled_flag;
-		uint8_t  pps_dbf_info_in_ph_flag;
-
-		int8_t   pps_luma_beta_offset_div2;
-		int8_t   pps_luma_tc_offset_div2;
-		int8_t   pps_cb_beta_offset_div2;
-		int8_t   pps_cb_tc_offset_div2;
-		int8_t   pps_cr_beta_offset_div2;
-		int8_t   pps_cr_tc_offset_div2;
-
-		uint8_t  pps_rpl_info_in_ph_flag;
-		uint8_t  pps_sao_info_in_ph_flag;
-		uint8_t  pps_alf_info_in_ph_flag;
-		uint8_t  pps_wp_info_in_ph_flag;
-		uint8_t  pps_qp_delta_info_in_ph_flag;
-
-		uint8_t  pps_picture_header_extension_present_flag;
-		uint8_t  pps_slice_header_extension_present_flag;
-		uint8_t  pps_extension_flag;
-
-		//calculated value;
-		uint16_t num_tile_columns;
-		uint16_t num_tile_rows;
-		uint16_t num_tiles_in_pic;
-		uint16_t slice_height_in_ctus[VVC_MAX_SLICES];
-		uint16_t num_slices_in_subpic[VVC_MAX_SLICES];
-		uint16_t sub_pic_id_val[VVC_MAX_SLICES];
-		uint16_t col_width_val[VVC_MAX_TILE_COLUMNS];
-		uint16_t row_height_val[VVC_MAX_TILE_ROWS];
 	};
 
 #define bit_position(gb) (gb.GetBitsPos())
@@ -2587,15 +2500,15 @@ namespace VVCParser {
 		return 0;
 	}
 
+	constexpr uint8_t h266_sub_width_c[] = {
+		1, 2, 2, 1
+	};
+	constexpr uint8_t h266_sub_height_c[] = {
+		1, 2, 1, 1
+	};
+
 	bool ParseSequenceParameterSet(const BYTE* data, const int size, vc_params_vvc_t& params)
 	{
-		static const uint8_t h266_sub_width_c[] = {
-			1, 2, 2, 1
-		};
-		static const uint8_t h266_sub_height_c[] = {
-			1, 2, 1, 1
-		};
-
 		CGolombBuffer gb(data, size, true);
 		auto rawSPS = std::make_unique<H266RawSPS>();
 		auto current = rawSPS.get();
@@ -3169,15 +3082,8 @@ namespace VVCParser {
 	bool ParsePictureParameterSet(const BYTE* data, const int size, vc_params_vvc_t& params)
 	{
 		CGolombBuffer gb(data, size, true);
-		auto rawPPS = std::make_unique<H266RawPPS>();
-		auto current = rawPPS.get();
-
-		static const uint8_t h266_sub_width_c[] = {
-			1, 2, 2, 1
-		};
-		static const uint8_t h266_sub_height_c[] = {
-			1, 2, 1, 1
-		};
+		H266RawPPS pps = {};
+		auto current = &pps;
 
 		ub(6, pps_pic_parameter_set_id);
 		ub(4, pps_seq_parameter_set_id);
@@ -3255,13 +3161,11 @@ namespace VVCParser {
 			}
 		}
 
-		auto pps = rawPPS.get();
-
-		params.width = pps->pps_pic_width_in_luma_samples -
-			(pps->pps_conf_win_left_offset + pps->pps_conf_win_right_offset) *
+		params.width = pps.pps_pic_width_in_luma_samples -
+			(pps.pps_conf_win_left_offset + pps.pps_conf_win_right_offset) *
 			h266_sub_width_c[params.sps_chroma_format_idc];
-		params.height = pps->pps_pic_height_in_luma_samples -
-			(pps->pps_conf_win_top_offset + pps->pps_conf_win_bottom_offset) *
+		params.height = pps.pps_pic_height_in_luma_samples -
+			(pps.pps_conf_win_top_offset + pps.pps_conf_win_bottom_offset) *
 			h266_sub_height_c[params.sps_chroma_format_idc];
 
 		return true;
