d8bb9246842006a797aa90cd0b430962561d4ecc
v0lt <mpc-be@mail.ru>
2024-01-13 14:47:56 +0300

    Убираем лишние нули времени для эксклюзивного экрана.

--- src/apps/mplayerc/OSD.cpp
+++ src/apps/mplayerc/OSD.cpp
@@ -1,5 +1,5 @@
 /*
- * (C) 2006-2023 see Authors.txt
+ * (C) 2006-2024 see Authors.txt
  *
  * This file is part of MPC-BE.
  *
@@ -365,10 +365,12 @@ void COSD::DrawSeekbar()
 	DrawRect(rect, &m_brushBar);
 
 	if (m_SeekbarFont.GetSafeHandle()) {
-		CStringW text = ReftimeToString2(m_llSeekPos);
+		const bool bShowZeroHours = HourOrMore(m_llSeekStop > 0 ? m_llSeekStop : m_llSeekPos);
+
+		CStringW text = ReftimeToString2(m_llSeekPos, bShowZeroHours);
 		if (m_llSeekStop > 0) {
 			text.Append(L" / ");
-			text.Append(ReftimeToString2(m_llSeekStop));
+			text.Append(ReftimeToString2(m_llSeekStop, bShowZeroHours));
 		}
 
 		m_MemDC.SelectObject(m_SeekbarFont);
