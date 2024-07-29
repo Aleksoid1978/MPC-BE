dcdde306794418ffed923a8226909662cfe2913e
v0lt <mpc-be@mail.ru>
2024-07-29 12:29:36 +0300

    FFmpeg: исправлена компиляция внешних фильтров с HAVE_UNISTD_H равным 1.

--- src/filters/ffmpeg_link_fix.h
+++ src/filters/ffmpeg_link_fix.h
@@ -5,5 +5,10 @@
 void *__imp__aligned_malloc  = _aligned_malloc;
 void *__imp__aligned_realloc = _aligned_realloc;
 void *__imp__aligned_free    = _aligned_free;
+#ifdef REGISTER_FILTER
+#include <io.h>
+void *__imp__sopen  = _sopen;
+void *__imp__wsopen = _wsopen;
+#endif
 
 #endif // FFMPEG_LINK_FIX_H
