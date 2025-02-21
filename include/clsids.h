04522d03cc12bc409a1a66df10a347cd5b9f3163
v0lt <mpc-be@mail.ru>
2025-02-21 18:59:00 +0300

    Добавлено описание CLSID_NullAudioRenderer и CLSID_NullUAudioRenderer.

--- include/clsids.h
+++ include/clsids.h
@@ -1,5 +1,5 @@
 /*
- * (C) 2020-2024 see Authors.txt
+ * (C) 2020-2025 see Authors.txt
  *
  * This file is part of MPC-BE.
  *
@@ -93,17 +93,26 @@ DEFINE_GUID(CLSID_MPCVR,
 
 // audio renderers
 
-// {601D2A2B-9CDE-40bd-8650-0485E3522727} - MPC Audio Renderer(WASAPI)
+// {601D2A2B-9CDE-40bd-8650-0485E3522727} MPC Audio Renderer(WASAPI)
 DEFINE_GUID(CLSID_MpcAudioRenderer,
 			0x601d2a2b, 0x9cde, 0x40bd, 0x86, 0x50, 0x04, 0x85, 0xe3, 0x52, 0x27, 0x27);
 
-// {9DC15360-914C-46B8-B9DF-BFE67FD36C6A} - ReClock
+// {9DC15360-914C-46B8-B9DF-BFE67FD36C6A} ReClock
 DEFINE_GUID(CLSID_ReClock,
 			0x9dc15360, 0x914c, 0x46b8, 0xb9, 0xdf, 0xbf, 0xe6, 0x7f, 0xd3, 0x6c, 0x6a);
 
 // {DF557071-C9FD-433A-9627-81E0D3640ED9}
 DEFINE_GUID(CLSID_SanearAudioRenderer,
 			0xdf557071, 0xc9fd, 0x433a, 0x96, 0x27, 0x81, 0xe0, 0xd3, 0x64, 0x0e, 0xd9);
+
+// {0C38BDFD-8C17-4E00-A344-F89397D3E22A} Null Audio Renderer (Any)
+DEFINE_GUID(CLSID_NullAudioRenderer,
+			0x0C38BDFD, 0x8C17, 0x4E00, 0xA3, 0x44, 0xF8, 0x93, 0x97, 0xD3, 0xE2, 0x2A);
+
+// {64A45125-7343-4772-9DA4-179FAC9D462C"} Null Audio Renderer (Uncompressed)
+DEFINE_GUID(CLSID_NullUAudioRenderer,
+			0x64A45125, 0x7343, 0x4772, 0x9D, 0xA4, 0x17, 0x9F, 0xAC, 0x9D, 0x46, 0x2C);
+
 
 // subtitle renderers
 
