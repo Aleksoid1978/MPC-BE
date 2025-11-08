67da389ada5fe8cfdb424b4e150bdf49934a1110
v0lt <mpc-be@mail.ru>
2025-11-08 22:32:34 +0300

    Небольшая корректировка DEFAULT_AUDIO_PATHS и DEFAULT_SUBTITLE_PATHS.

--- src/apps/mplayerc/AppSettings.h
+++ src/apps/mplayerc/AppSettings.h
@@ -170,8 +170,8 @@ enum MCE_RAW_INPUT {
 #define AUDRNDT_NULL_UNCOMP L"Null Audio Renderer (Uncompressed)"
 #define AUDRNDT_MPC         L"MPC Audio Renderer"
 
-#define DEFAULT_AUDIO_PATHS    L".;.\\audio;.\\fandub"
-#define DEFAULT_SUBTITLE_PATHS L".;.\\subtitles;.\\subs;.\\*sub"
+#define DEFAULT_AUDIO_PATHS    L".;.\\audio;.\\fandub;"
+#define DEFAULT_SUBTITLE_PATHS L".;.\\subtitles;.\\*subs;.\\*sub;"
 #define DEFAULT_JUMPDISTANCE_1  1000
 #define DEFAULT_JUMPDISTANCE_2  5000
 #define DEFAULT_JUMPDISTANCE_3 20000
