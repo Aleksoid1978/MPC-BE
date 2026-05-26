b94e2a50e9653d93e4c81b59e5392f17816e055b
v0lt <mpc-be@mail.ru>
2026-05-26 18:59:00 +0300

    MediaInfo: Используем оригинальный TimeCode.cpp без патчей.

--- src/ExtLib/MediaInfo/MediaInfo/TimeCode.cpp
+++ src/ExtLib/MediaInfo/MediaInfo/TimeCode.cpp
@@ -6,14 +6,6 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 */
 
 //---------------------------------------------------------------------------
-// Pre-compilation
-#include "MediaInfo/PreComp.h"
-#ifdef __BORLANDC__
-#pragma hdrstop
-#endif
-//---------------------------------------------------------------------------
-
-//---------------------------------------------------------------------------
 #include "TimeCode.h"
 #include <limits>
 //---------------------------------------------------------------------------
